186b71d8e5cb3a82ee23cd7c0e09fa310efe62ff
Angelo De Caro <adc@zurich.ibm.com>
2021-07-06 13:54:26 +0200

    two layers (token/driver api) issue options
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    Signed-off-by: KAO <kao@zurich.ibm.com>

--- token/driver/issue.go
+++ token/driver/issue.go
@@ -7,14 +7,18 @@ package driver
 
 import "github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
 
+// IssueOptions models the options that can be passed to the issue command
 type IssueOptions struct {
-	mapping map[interface{}]interface{}
+	// Attributes is a container of generic options that might be driver specific
+	Attributes map[interface{}]interface{}
 }
 
-type IssueOption func(*IssueOptions) error
-
+// IssueService models the token issue service
 type IssueService interface {
-	Issue(id view.Identity, typ string, values []uint64, owners [][]byte, opts ...IssueOption) (IssueAction, [][]byte, view.Identity, error)
+	// Issue generates an IssuerAction whose tokens are issued by the passed identity.
+	// The tokens to be issued are passed as pairs (value, owner).
+	// In addition, a set of options can be specified to further customized the issue command
+	Issue(id view.Identity, typ string, values []uint64, owners [][]byte, opts *IssueOptions) (IssueAction, [][]byte, view.Identity, error)
 
 	VerifyIssue(tr IssueAction, tokenInfos [][]byte) error
 
