7c60685ab17fffbfda0c268794c2180abb4d5c0b
Angelo De Caro <adc@zurich.ibm.com>
2021-09-20 10:12:49 +0200

    fix module dependency
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ tools.go
@@ -0,0 +1,18 @@
+// +build tools
+
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package tools
+
+import (
+	_ "github.com/hyperledger-labs/fabric-smart-client/cmd/cryptogen"
+	_ "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/services/weaver/relay/fabric"
+	_ "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm@"
+	_ "github.com/hyperledger/fabric/common/metrics/prometheus"
+	_ "github.com/hyperledger/fabric/core/operations"
+	_ "github.com/libp2p/go-libp2p-core/network"
+)
