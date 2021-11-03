87c55daaaaaafcf5f109ede403792bcac431c43f
Angelo De Caro <adc@zurich.ibm.com>
2021-11-03 12:42:51 +0100

    fix unit-tests
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/tcc/tcc.go
+++ token/services/tcc/tcc.go
@@ -275,11 +275,10 @@ func (cc *TokenChaincode) invoke(raw []byte, stub shim.ChaincodeStubInterface) p
 			return shim.Error("failed to write token action: " + err.Error())
 		}
 	}
-	// TODO: add an option to commit the token request
-	// err = w.CommitTokenRequest(raw)
-	// if err != nil {
-	// 	return shim.Error("failed to write token request:" + err.Error())
-	// }
+	err = w.CommitTokenRequest(raw)
+	if err != nil {
+		return shim.Error("failed to write token request:" + err.Error())
+	}
 	return shim.Success(nil)
 }
 
