454f4f6ac24468cd9b07910023844ba0d20b3180
Angelo De Caro <adc@zurich.ibm.com>
2022-02-25 13:10:52 +0100

    - public params with list of issuers - first skeleton of dvp sample
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ samples/fabric/dvp/topology/topology.go
@@ -0,0 +1,15 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package topology
+
+import (
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/api"
+)
+
+func Topology(tokenSDKDriver string) []api.Topology {
+	panic("not implemented")
+}
