f316310193c3db475a341fa2b09b9b4711b50ee8
Angelo De Caro <adc@zurich.ibm.com>
2022-05-03 13:18:40 +0200

    tokengen should take in input issuer and auditor public keys #232
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- cmd/tokengen/main_test.go
+++ cmd/tokengen/main_test.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
+	"os/exec"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -18,4 +19,49 @@ func TestCompile(t *testing.T) {
 	_, err := gexec.Build("github.com/hyperledger-labs/fabric-token-sdk/cmd/tokengen")
 	gt.Expect(err).NotTo(HaveOccurred())
 	defer gexec.CleanupBuildArtifacts()
+}
+
+func TestGen(t *testing.T) {
+	gt := NewGomegaWithT(t)
+	tokengen, err := gexec.Build("github.com/hyperledger-labs/fabric-token-sdk/cmd/tokengen")
+	gt.Expect(err).NotTo(HaveOccurred())
+	defer gexec.CleanupBuildArtifacts()
+
+	// auditors
+	args := []string{
+		"gen",
+		"-d", "fabtoken",
+		"--auditors", "a:Org1MSP,b:Org2MSP",
+	}
+	b, err := exec.Command(tokengen, args...).CombinedOutput()
+	gt.Expect(err).To(HaveOccurred())
+	gt.Expect(string(b)).To(ContainSubstring("Error: failed to create x509 provider for auditor [a:Org1MSP]: could not load a valid signer certificate from directory a/signcerts: stat a/signcerts: no such file or directory"))
+
+	args = []string{
+		"gen",
+		"-d", "fabtoken",
+		"--auditors", "aOrg1MSP,b:Org2MSP",
+	}
+	b, err = exec.Command(tokengen, args...).CombinedOutput()
+	gt.Expect(err).To(HaveOccurred())
+	gt.Expect(string(b)).To(ContainSubstring("Error: invalid auditor [aOrg1MSP]"))
+
+	// issuers
+	args = []string{
+		"gen",
+		"-d", "fabtoken",
+		"--issuers", "a:Org1MSP,b:Org2MSP",
+	}
+	b, err = exec.Command(tokengen, args...).CombinedOutput()
+	gt.Expect(err).To(HaveOccurred())
+	gt.Expect(string(b)).To(ContainSubstring("Error: failed to create x509 provider for issuer [a:Org1MSP]: could not load a valid signer certificate from directory a/signcerts: stat a/signcerts: no such file or directory"))
+
+	args = []string{
+		"gen",
+		"-d", "fabtoken",
+		"--issuers", "aOrg1MSP,b:Org2MSP",
+	}
+	b, err = exec.Command(tokengen, args...).CombinedOutput()
+	gt.Expect(err).To(HaveOccurred())
+	gt.Expect(string(b)).To(ContainSubstring("Error: invalid issuer [aOrg1MSP]"))
 }
