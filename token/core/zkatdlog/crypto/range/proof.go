a28e72d6d6cae9945fb5ba72511fe8d264395f25
Kaoutar <kao@zurich.ibm.com>
2022-06-16 17:16:23 +0200

    refactor range proof
    Signed-off-by: Kaoutar <kao@zurich.ibm.com>

--- token/core/zkatdlog/crypto/range/proof.go
+++ token/core/zkatdlog/crypto/range/proof.go
@@ -19,10 +19,10 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Proof is a range proof that  show that an array of token have value < max_value
+// RangeProof is a range proof that  show that an array of token have value < max_value
 // max_value = base^exponent - 1
 // token_value = \sum_{i=0}^exponent v_i base^i and 0=<v_i =<base-1
-type Proof struct {
+type RangeProof struct {
 	// Challenge used to compute the proof
 	Challenge *mathlib.Zr
 	// EqualityProofs show that for each token in an array of tokens,
@@ -47,7 +47,7 @@ type EqualityProofs struct {
 	CommitmentBlindingFactor []*mathlib.Zr
 }
 
-// MembershipProof shows that committed values 0=<v_i =<base-1, for 1 =< i =< n
+// MembershipProof shows that committed values 0=<v_i =<max_value, for 1 =< i =< n
 type MembershipProof struct {
 	// Commitments is an array of Pedersen commitments
 	Commitments []*mathlib.G1
@@ -71,7 +71,7 @@ func NewProver(tw []*token.TokenDataWitness, token []*mathlib.G1, signatures []*
 		tokenWitness: tw,
 		Signatures:   signatures,
 		Verifier: &Verifier{
-			Token:          token,
+			Tokens:         token,
 			Base:           uint64(len(signatures)),
 			Exponent:       exponent,
 			PedersenParams: pp,
@@ -85,8 +85,8 @@ func NewProver(tw []*token.TokenDataWitness, token []*mathlib.G1, signatures []*
 
 // Verifier checks the validity of range proofs produced by Prover
 type Verifier struct {
-	// Token is an array of TokenData - commitment to (value, type)
-	Token []*mathlib.G1
+	// Tokens is an array of TokenData - commitment to (value, type)
+	Tokens []*mathlib.G1
 	// max_value = Base^Exponent
 	Base     uint64
 	Exponent int
@@ -105,7 +105,7 @@ type Verifier struct {
 // NewVerifier returns a range proof Verifier
 func NewVerifier(token []*mathlib.G1, base uint64, exponent int, pp []*mathlib.G1, PK []*mathlib.G2, P *mathlib.G1, Q *mathlib.G2, c *mathlib.Curve) *Verifier {
 	return &Verifier{
-		Token:          token,
+		Tokens:         token,
 		Base:           base,
 		Exponent:       exponent,
 		PedersenParams: pp,
@@ -116,29 +116,30 @@ func NewVerifier(token []*mathlib.G1, base uint64, exponent int, pp []*mathlib.G
 	}
 }
 
-// Randomness is the randomness used in the range proof
-type Randomness struct {
-	Type                     *mathlib.Zr
-	Value                    []*mathlib.Zr
-	TokenBlindingFactor      []*mathlib.Zr
-	CommitmentBlindingFactor []*mathlib.Zr
+// randomness is the randomness used in the range proof
+type randomness struct {
+	tokenType                         *mathlib.Zr
+	values                            []*mathlib.Zr
+	tokensBlindingFactors             []*mathlib.Zr
+	commitmentsToValueBlindingFactors []*mathlib.Zr
 }
 
 // Commitment is the commitment to the randomness used in the range proof
 type Commitment struct {
-	Token             []*mathlib.G1
-	CommitmentToValue []*mathlib.G1
+	Tokens              []*mathlib.G1
+	CommitmentsToValues []*mathlib.G1
 }
 
-// ?
-type commitmentWitnessBlindingFactor struct {
-	commitment     [][]*mathlib.G1
-	witness        [][]*sigproof.MembershipWitness
-	blindingFactor []*mathlib.Zr
+// rangeProofWitness is the secret information needed to generate a range proof
+type rangeProofWitness struct {
+	commitmentsToValues                [][]*mathlib.G1
+	membershipWitnesses                [][]*sigproof.MembershipWitness
+	commitmentsToValuesBlindingFactors []*mathlib.Zr
 }
 
+// Prove generates a range proof
 func (p *Prover) Prove() ([]byte, error) {
-	proof := &Proof{}
+	proof := &RangeProof{}
 	var err error
 	preProcessed, err := p.preProcess()
 	if err != nil {
@@ -146,18 +147,19 @@ func (p *Prover) Prove() ([]byte, error) {
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(len(p.Token) * p.Exponent)
+	wg.Add(len(p.Tokens) * p.Exponent)
 
 	parallelErr := &atomic.Value{}
 
-	proof.MembershipProofs = make([]*MembershipProof, len(p.Token))
+	// produce proof that each committed value is signed
+	proof.MembershipProofs = make([]*MembershipProof, len(p.Tokens))
 	for k := 0; k < len(proof.MembershipProofs); k++ {
 		proof.MembershipProofs[k] = &MembershipProof{}
 		proof.MembershipProofs[k].Commitments = make([]*mathlib.G1, p.Exponent)
 		proof.MembershipProofs[k].SignatureProofs = make([][]byte, p.Exponent)
 		for i := 0; i < p.Exponent; i++ {
-			proof.MembershipProofs[k].Commitments[i] = preProcessed.commitment[k][i]
-			mp := sigproof.NewMembershipProver(preProcessed.witness[k][i], proof.MembershipProofs[k].Commitments[i], p.P, p.Q, p.PK, p.PedersenParams[:2], p.Curve)
+			proof.MembershipProofs[k].Commitments[i] = preProcessed.commitmentsToValues[k][i]
+			mp := sigproof.NewMembershipProver(preProcessed.membershipWitnesses[k][i], proof.MembershipProofs[k].Commitments[i], p.P, p.Q, p.PK, p.PedersenParams[:2], p.Curve)
 
 			go func(k, i int) {
 				defer wg.Done()
@@ -176,20 +178,22 @@ func (p *Prover) Prove() ([]byte, error) {
 		return nil, parallelErr.Load().(error)
 	}
 
-	// show that value in token = value in the aggregate commitment
+	// show that value in token = \prod_{i=0}^Exponent com_i^{Base^i}
 	commitment, randomness, err := p.computeCommitment()
 	if err != nil {
 		return nil, err
 	}
 
-	proof.Challenge, err = p.computeChallenge(commitment, preProcessed.commitment)
+	proof.Challenge, err = p.computeChallenge(commitment, preProcessed.commitmentsToValues)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to compute range proof")
 	}
+
 	// equality proof
+	// value in token = \prod_{i=0}^Exponent com_i^{Base^i}
 	proof.EqualityProofs = &EqualityProofs{}
-	for k := 0; k < len(p.Token); k++ {
-		sp := &common.SchnorrProver{Challenge: proof.Challenge, Randomness: []*mathlib.Zr{randomness.Value[k], randomness.TokenBlindingFactor[k], randomness.CommitmentBlindingFactor[k]}, Witness: []*mathlib.Zr{p.tokenWitness[k].Value, p.tokenWitness[k].BlindingFactor, preProcessed.blindingFactor[k]}, SchnorrVerifier: &common.SchnorrVerifier{Curve: p.Curve}}
+	for k := 0; k < len(p.Tokens); k++ {
+		sp := &common.SchnorrProver{Challenge: proof.Challenge, Randomness: []*mathlib.Zr{randomness.values[k], randomness.tokensBlindingFactors[k], randomness.commitmentsToValueBlindingFactors[k]}, Witness: []*mathlib.Zr{p.tokenWitness[k].Value, p.tokenWitness[k].BlindingFactor, preProcessed.commitmentsToValuesBlindingFactors[k]}, SchnorrVerifier: &common.SchnorrVerifier{Curve: p.Curve}}
 		proofs, err := sp.Prove()
 		if err != nil {
 			return nil, err
@@ -199,28 +203,29 @@ func (p *Prover) Prove() ([]byte, error) {
 		proof.EqualityProofs.CommitmentBlindingFactor = append(proof.EqualityProofs.CommitmentBlindingFactor, proofs[2])
 	}
 	proof.EqualityProofs.Type = p.Curve.ModMul(proof.Challenge, p.Curve.HashToZr([]byte(p.tokenWitness[0].Type)), p.Curve.GroupOrder)
-	proof.EqualityProofs.Type = p.Curve.ModAdd(proof.EqualityProofs.Type, randomness.Type, p.Curve.GroupOrder)
+	proof.EqualityProofs.Type = p.Curve.ModAdd(proof.EqualityProofs.Type, randomness.tokenType, p.Curve.GroupOrder)
 
 	return json.Marshal(proof)
 }
 
 func (v *Verifier) Verify(raw []byte) error {
 	// todo check length of public parameters
-	proof := &Proof{}
+	proof := &RangeProof{}
 	err := json.Unmarshal(raw, proof)
 	if err != nil {
 		return err
 	}
 
-	if len(proof.MembershipProofs) != len(v.Token) {
+	if len(proof.MembershipProofs) != len(v.Tokens) {
 		return errors.Errorf("range proof not well formed")
 	}
 
 	var verifications []func()
 	parallelErr := &atomic.Value{}
 
-	//  verify membership
-	for k := 0; k < len(v.Token); k++ {
+	// verify membership
+	// each committed value v_i is signed (i.e., v_i < Base)
+	for k := 0; k < len(v.Tokens); k++ {
 		if proof.MembershipProofs[k] == nil {
 			return errors.Errorf("range proof not well formed")
 		}
@@ -255,7 +260,8 @@ func (v *Verifier) Verify(raw []byte) error {
 		return parallelErr.Load().(error)
 	}
 
-	//  verify equality
+	// verify equality proof
+	// value in token = \prod_{i=0}^Exponent com_i^{Base^i}
 	com, err := v.recomputeCommitments(proof)
 	if err != nil {
 		return err
@@ -277,7 +283,9 @@ func (v *Verifier) Verify(raw []byte) error {
 	return nil
 }
 
-func (p *Prover) preProcess() (*commitmentWitnessBlindingFactor, error) {
+// preProcess computes commitment to values v_i such that v = v = \sum_{i=0}^Exponent values[i] Base^i
+// preProcess returns the corresponding rangeProofWitness
+func (p *Prover) preProcess() (*rangeProofWitness, error) {
 	rand, err := p.Curve.Rand()
 	if err != nil {
 		return nil, err
@@ -295,66 +303,73 @@ func (p *Prover) preProcess() (*commitmentWitnessBlindingFactor, error) {
 		if v >= int64(math.Pow(float64(p.Base), float64(p.Exponent))) {
 			return nil, errors.Errorf("can't compute range proof: value of token outside authorized range")
 		}
+		// v = \sum_{i=0}^Exponent values[i] Base^i
 		values[0] = int(v % int64(p.Base))
 		for i := 0; i < p.Exponent-1; i++ {
 			values[p.Exponent-1-i] = int(v / int64(math.Pow(float64(p.Base), float64(p.Exponent-1-i)))) // quotient
 			v = v % int64(math.Pow(float64(p.Base), float64(p.Exponent-1-i)))                           // remainder
 		}
 
+		// generate witness for membership of values[i]
+		// membership corresponds to a Pointcheval-Sanders signature
 		membershipWitness[k] = make([]*sigproof.MembershipWitness, p.Exponent)
 		commitmentBlindingFactor[k] = p.Curve.NewZrFromInt(0)
 		coms[k] = make([]*mathlib.G1, p.Exponent)
 		for i := 0; i < p.Exponent; i++ {
 			bf := p.Curve.NewRandomZr(rand)
+			// compute Pedersen commitment to values[i]
 			coms[k][i], err = common.ComputePedersenCommitment([]*mathlib.Zr{p.Curve.NewZrFromInt(int64(values[i])), bf}, p.PedersenParams[:2], p.Curve)
 			if err != nil {
 				return nil, err
 			}
-
+			// membershipWitness contains Pointcheval-Sanders signature of values[i], values[i] and the blinding factor used to compute the commitment to values[i]
 			membershipWitness[k][i] = sigproof.NewMembershipWitness(p.Signatures[values[i]], p.Curve.NewZrFromInt(int64(values[i])), bf)
 			pow := p.Curve.NewZrFromInt(int64(math.Pow(float64(p.Base), float64(i))))
+			// this is the blinding factor to commitment  c = \prod_{i=0}^Exponent com[k][i]^{Base^i}
 			commitmentBlindingFactor[k] = p.Curve.ModAdd(commitmentBlindingFactor[k], p.Curve.ModMul(bf, pow, p.Curve.GroupOrder), p.Curve.GroupOrder)
 		}
 	}
-	return &commitmentWitnessBlindingFactor{
-		commitment:     coms,
-		blindingFactor: commitmentBlindingFactor,
-		witness:        membershipWitness,
+	return &rangeProofWitness{
+		commitmentsToValues:                coms,
+		commitmentsToValuesBlindingFactors: commitmentBlindingFactor,
+		membershipWitnesses:                membershipWitness,
 	}, nil
 }
 
-func (p *Prover) computeCommitment() (*Commitment, *Randomness, error) {
+// computeCommitment generates range proof randomness and computes the corresponding commitments
+func (p *Prover) computeCommitment() (*Commitment, *randomness, error) {
 	rand, err := p.Curve.Rand()
 	if err != nil {
 		return nil, nil, err
 	}
 	// generate randomness
-	randomness := &Randomness{}
-	randomness.Type = p.Curve.NewRandomZr(rand)
-	for i := 0; i < len(p.Token); i++ {
-		randomness.Value = append(randomness.Value, p.Curve.NewRandomZr(rand))
-		randomness.CommitmentBlindingFactor = append(randomness.CommitmentBlindingFactor, p.Curve.NewRandomZr(rand))
-		randomness.TokenBlindingFactor = append(randomness.TokenBlindingFactor, p.Curve.NewRandomZr(rand))
+	randomness := &randomness{}
+	randomness.tokenType = p.Curve.NewRandomZr(rand)
+	for i := 0; i < len(p.Tokens); i++ {
+		randomness.values = append(randomness.values, p.Curve.NewRandomZr(rand))
+		randomness.commitmentsToValueBlindingFactors = append(randomness.commitmentsToValueBlindingFactors, p.Curve.NewRandomZr(rand))
+		randomness.tokensBlindingFactors = append(randomness.tokensBlindingFactors, p.Curve.NewRandomZr(rand))
 	}
 
 	// compute commitment
 	commitment := &Commitment{}
 	for i := 0; i < len(p.tokenWitness); i++ {
-		tok := p.PedersenParams[0].Mul(randomness.Type)
-		tok.Add(p.PedersenParams[1].Mul(randomness.Value[i]))
-		tok.Add(p.PedersenParams[2].Mul(randomness.TokenBlindingFactor[i]))
-		commitment.Token = append(commitment.Token, tok)
+		tok := p.PedersenParams[0].Mul(randomness.tokenType)
+		tok.Add(p.PedersenParams[1].Mul(randomness.values[i]))
+		tok.Add(p.PedersenParams[2].Mul(randomness.tokensBlindingFactors[i]))
+		commitment.Tokens = append(commitment.Tokens, tok)
 
-		com := p.PedersenParams[0].Mul(randomness.Value[i])
-		com.Add(p.PedersenParams[1].Mul(randomness.CommitmentBlindingFactor[i]))
-		commitment.CommitmentToValue = append(commitment.CommitmentToValue, com)
+		com := p.PedersenParams[0].Mul(randomness.values[i])
+		com.Add(p.PedersenParams[1].Mul(randomness.commitmentsToValueBlindingFactors[i]))
+		commitment.CommitmentsToValues = append(commitment.CommitmentsToValues, com)
 	}
 
 	return commitment, randomness, nil
 }
 
+// computeChallenge computes the challenge for the range proof
 func (v *Verifier) computeChallenge(commitment *Commitment, comToValue [][]*mathlib.G1) (*mathlib.Zr, error) {
-	g1array, err := common.GetG1Array([]*mathlib.G1{v.P}, v.Token, commitment.Token, commitment.CommitmentToValue, v.PedersenParams).Bytes()
+	g1array, err := common.GetG1Array([]*mathlib.G1{v.P}, v.Tokens, commitment.Tokens, commitment.CommitmentsToValues, v.PedersenParams).Bytes()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to compute challenge")
 	}
@@ -373,33 +388,35 @@ func (v *Verifier) computeChallenge(commitment *Commitment, comToValue [][]*math
 	return v.Curve.HashToZr(bytes), nil
 }
 
-func (v *Verifier) recomputeCommitments(p *Proof) (*Commitment, error) {
+// recomputeCommitments computes the commitment to randomness used in the proof as a function
+// of the proof
+func (v *Verifier) recomputeCommitments(p *RangeProof) (*Commitment, error) {
 	if p.EqualityProofs == nil {
 		return nil, errors.Errorf("range proof not well formed")
 	}
-	if len(p.EqualityProofs.Value) != len(v.Token) {
+	if len(p.EqualityProofs.Value) != len(v.Tokens) {
 		return nil, errors.Errorf("range proof not well formed")
 	}
-	if len(p.EqualityProofs.TokenBlindingFactor) != len(v.Token) {
+	if len(p.EqualityProofs.TokenBlindingFactor) != len(v.Tokens) {
 		return nil, errors.Errorf("range proof not well formed")
 	}
-	if len(p.EqualityProofs.CommitmentBlindingFactor) != len(v.Token) {
+	if len(p.EqualityProofs.CommitmentBlindingFactor) != len(v.Tokens) {
 		return nil, errors.Errorf("range proof not well formed")
 	}
 
 	c := &Commitment{}
 	// recompute commitments for verification
-	for j := 0; j < len(v.Token); j++ {
+	for j := 0; j < len(v.Tokens); j++ {
 		ver := &common.SchnorrVerifier{PedParams: v.PedersenParams, Curve: v.Curve}
-		zkp := &common.SchnorrProof{Statement: v.Token[j], Proof: []*mathlib.Zr{p.EqualityProofs.Type, p.EqualityProofs.Value[j], p.EqualityProofs.TokenBlindingFactor[j]}, Challenge: p.Challenge}
+		zkp := &common.SchnorrProof{Statement: v.Tokens[j], Proof: []*mathlib.Zr{p.EqualityProofs.Type, p.EqualityProofs.Value[j], p.EqualityProofs.TokenBlindingFactor[j]}, Challenge: p.Challenge}
 		com, err := ver.RecomputeCommitment(zkp)
 		if err != nil {
 			return nil, err
 		}
-		c.Token = append(c.Token, com)
+		c.Tokens = append(c.Tokens, com)
 	}
 
-	for j := 0; j < len(v.Token); j++ {
+	for j := 0; j < len(v.Tokens); j++ {
 		com := v.Curve.NewG1()
 		if p.MembershipProofs[j] == nil {
 			return nil, errors.Errorf("range proof not well formed")
@@ -421,7 +438,7 @@ func (v *Verifier) recomputeCommitments(p *Proof) (*Commitment, error) {
 		if err != nil {
 			return nil, err
 		}
-		c.CommitmentToValue = append(c.CommitmentToValue, com)
+		c.CommitmentsToValues = append(c.CommitmentsToValues, com)
 	}
 	return c, nil
 }
