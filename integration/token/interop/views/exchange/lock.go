b73b1f8f6186c9dea28f9271f5e08a85a86a1e92
Hagar Meir <hagar.meir@ibm.com>
2022-06-20 14:49:27 +0300

    add lock view
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

---
+++ integration/token/interop/views/exchange/lock.go
@@ -0,0 +1,162 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package exchange
+
+import (
+	"crypto"
+	"encoding/json"
+	"time"
+
+	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric"
+	view2 "github.com/hyperledger-labs/fabric-smart-client/platform/view"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/assert"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
+	"github.com/hyperledger-labs/fabric-token-sdk/token"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/interop/encoding"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/interop/exchange"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/ttx"
+)
+
+type Lock struct {
+	// TMSID identifies the TMS to use to perform the token operation
+	TMSID token.TMSID
+	// ReclamationDeadline is the time after which we can reclaim the funds in case they were not transferred
+	ReclamationDeadline time.Duration
+	// Wallet is the identifier of the wallet that owns the tokens to transfer
+	Wallet string
+	// Type of tokens to transfer
+	Type string
+	// Amount to transfer
+	Amount uint64
+	// Recipient is the identity of the recipient's FSC node
+	Recipient view.Identity
+	// Hash is the hash to use in the script, if nil, a fresh one is generated
+	Hash []byte
+	// HashFunc is the hash function to use in the script
+	HashFunc crypto.Hash
+}
+
+type LockResult struct {
+	TxID     string
+	PreImage []byte
+	Hash     []byte
+}
+
+type LockView struct {
+	*Lock
+}
+
+func (hv *LockView) Call(context view.Context) (interface{}, error) {
+	// As a first step operation, the sender contacts the recipient's FSC node
+	// to ask for the identity to use to assign ownership of the freshly created token.
+	// Notice that, this step would not be required if the sender knew already which
+	// identity the recipient wants to use.
+	me, recipient, err := ttx.ExchangeRecipientIdentities(context, "", hv.Recipient, token.WithTMSID(hv.TMSID))
+	assert.NoError(err, "failed getting recipient identity")
+
+	// At this point, the sender is ready to prepare the exchange transaction
+	// and specify the auditor that must be contacted to approve the operation.
+	tx, err := exchange.NewTransaction(
+		context,
+		fabric.GetIdentityProvider(context, hv.TMSID.Network).DefaultIdentity(),
+		ttx.WithAuditor(view2.GetIdentityProvider(context).Identity("auditor")),
+		ttx.WithTMSID(hv.TMSID),
+	)
+	assert.NoError(err, "failed creating an exchange transaction")
+
+	// The sender will select tokens owned by this wallet
+	senderWallet := ttx.GetWallet(context, hv.Wallet, token.WithTMSID(hv.TMSID))
+	assert.NotNil(senderWallet, "sender wallet [%s] not found", hv.Wallet)
+
+	// The sender adds an exchange operation to the transaction.
+	preImage, err := tx.Exchange(
+		senderWallet,
+		me,
+		hv.Type,
+		hv.Amount,
+		recipient,
+		hv.ReclamationDeadline,
+		exchange.WithHash(hv.Hash),
+		exchange.WithHashFunc(hv.HashFunc),
+		exchange.WithHashEncoding(encoding.None),
+	)
+	assert.NoError(err, "failed adding exchange")
+
+	// The sender is ready to collect all the required signatures.
+	// In this case, the sender's and the auditor's signatures.
+	// Invoke the Token Chaincode to collect endorsements on the Token Request and prepare the relative transaction.
+	// This is all done in one shot running the following view.
+	// Before completing, all recipients receive the approved transaction.
+	// Depending on the token driver implementation, the recipient's signature might or might not be needed to make
+	// the exchange transaction valid.
+	_, err = context.RunView(ttx.NewCollectEndorsementsView(tx.Transaction))
+	assert.NoError(err, "failed to collect endorsements on exchange transaction")
+
+	// Last but not least, the locker sends the transaction for ordering and waits for transaction finality.
+	_, err = context.RunView(ttx.NewOrderingAndFinalityView(tx.Transaction))
+	assert.NoError(err, "failed to commit issue transaction")
+
+	outputs, err := tx.Outputs()
+	assert.NoError(err, "failed getting outputs")
+
+	return &LockResult{
+		TxID:     tx.ID(),
+		PreImage: preImage,
+		Hash:     outputs.ScriptAt(0).Hash,
+	}, nil
+}
+
+type LockViewFactory struct{}
+
+func (p *LockViewFactory) NewView(in []byte) (view.View, error) {
+	f := &LockView{Lock: &Lock{}}
+	err := json.Unmarshal(in, f.Lock)
+	assert.NoError(err, "failed unmarshalling input")
+
+	return f, nil
+}
+
+type LockAcceptView struct {
+}
+
+func (h *LockAcceptView) Call(context view.Context) (interface{}, error) {
+	// The recipient of a token responds, as first operation,
+	// to a request for a recipient.
+	// The recipient can do that by using the following code.
+	// The recipient identity will be taken from the default wallet, if not otherwise specified.
+	me, sender, err := ttx.RespondExchangeRecipientIdentities(context)
+	assert.NoError(err, "failed to respond to identity request")
+
+	// At some point, the recipient receives the exchange transaction that in the meantime has been assembled
+	tx, err := exchange.ReceiveTransaction(context)
+	assert.NoError(err, "failed to receive tokens")
+
+	// The recipient can perform any check on the transaction as required by the business process
+	// In particular, here, the recipient checks that the transaction contains at least one output, and
+	// that there is at least one output that names the recipient. The recipient is receiving something.
+	outputs, err := tx.Outputs()
+	assert.NoError(err, "failed getting outputs")
+	assert.True(outputs.Count() >= 1, "expected at least one output, got [%d]", outputs.Count())
+	outputs = outputs.ByScript()
+	assert.True(outputs.Count() == 1, "expected only one exchange output, got [%d]", outputs.Count())
+	script := outputs.ScriptAt(0)
+	assert.NotNil(script, "expected an exchange script")
+	assert.True(me.Equal(script.Recipient), "expected me as recipient of the script")
+	assert.True(sender.Equal(script.Sender), "expected sender as sender of the script")
+
+	// If everything is fine, the recipient accepts and sends back her signature.
+	// Notice that, a signature from the recipient might or might not be required to make the transaction valid.
+	// This depends on the driver implementation.
+	_, err = context.RunView(ttx.NewAcceptView(tx.Transaction))
+	assert.NoError(err, "failed to accept new tokens")
+
+	// Before completing, the recipient waits for finality of the transaction
+	_, err = context.RunView(ttx.NewFinalityView(tx.Transaction))
+	assert.NoError(err, "new tokens were not committed")
+
+	return tx, nil
+}
