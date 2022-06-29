eb67fabd767b01e7abc4dc991b03dc166e68251f
Kaoutar <kao@zurich.ibm.com>
2022-06-29 10:57:25 +0200

    comments + nil checks actions.go and auditor.go
    Signed-off-by: Kaoutar <kao@zurich.ibm.com>

--- token/core/fabtoken/auditor.go
+++ token/core/fabtoken/auditor.go
@@ -8,6 +8,7 @@ package fabtoken
 
 import "github.com/hyperledger-labs/fabric-token-sdk/token/driver"
 
+// AuditorCheck verifies if the passed tokenRequest matches the tokenRequestMetadata
 func (s *Service) AuditorCheck(tokenRequest *driver.TokenRequest, tokenRequestMetadata *driver.TokenRequestMetadata, txID string) error {
 	// TODO:
 	return nil
