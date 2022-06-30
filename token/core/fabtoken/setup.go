f2655152cdb643c444f415d44ff006243633cda7
Kaoutar <kao@zurich.ibm.com>
2022-06-30 16:46:37 +0200

    add comments to fabtoken setup and validator and enhance error messages
    Signed-off-by: Kaoutar <kao@zurich.ibm.com>

--- token/core/fabtoken/setup.go
+++ token/core/fabtoken/setup.go
@@ -8,27 +8,36 @@ package fabtoken
 
 import (
 	"encoding/json"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
-	"github.com/pkg/errors"
 
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/driver"
+	"github.com/pkg/errors"
 )
 
 const (
-	Coin             = uint64(1000000000)
-	MaxMoney         = uint64(21000000) * Coin
+	Coin = uint64(1000000000)
+	// MaxMoney is the max value a token can hold
+	MaxMoney = uint64(21000000) * Coin
+	// PublicParameters is the key to be used to look up fabtoken parameters
 	PublicParameters = "fabtoken"
 	DefaultPrecision = uint64(64)
 )
 
 type PublicParams struct {
-	Label             string
-	MTV               uint64
+	// This indicates that this is the public parameters for fabtoken driver
+	Label string
+	// The maximum quantity a token can hold
+	MTV uint64
+	// The precision of token quantities
 	QuantityPrecision uint64
-	Auditor           []byte
-	Issuers           [][]byte
+	// This is set when audit is enabled
+	Auditor []byte
+	// This encodes the list of authorized auditors
+	Issuers [][]byte
 }
 
+// NewPublicParamsFromBytes deserializes the raw bytes into public parameters
+// The resulting public parameters are labeled with the passed label
 func NewPublicParamsFromBytes(raw []byte, label string) (*PublicParams, error) {
 	pp := &PublicParams{}
 	pp.Label = label
@@ -38,30 +47,41 @@ func NewPublicParamsFromBytes(raw []byte, label string) (*PublicParams, error) {
 	return pp, nil
 }
 
+// Identifier returns the label associated with the PublicParams
+// todo shall we used Identifier instead of Label?
 func (pp *PublicParams) Identifier() string {
 	return pp.Label
 }
 
+// TokenDataHiding indicates if the PublicParams corresponds to a driver that hides token data
+// fabtoken does not hide token data, hence, TokenDataHiding returns false
 func (pp *PublicParams) TokenDataHiding() bool {
 	return false
 }
 
+// CertificationDriver returns the label of the PublicParams
+// From the label, one can deduce what certification process will be used if any.
 func (pp *PublicParams) CertificationDriver() string {
 	return pp.Label
 }
 
+// GraphHiding indicates if the PublicParams corresponds to a driver that hides the transaction graph
+// fabtoken does not hide the graph, hence, GraphHiding returns false
 func (pp *PublicParams) GraphHiding() bool {
 	return false
 }
 
+// MaxTokenValue returns the maximum value that a token can hold according to PublicParams
 func (pp *PublicParams) MaxTokenValue() uint64 {
 	return pp.MTV
 }
 
+// Bytes marshals PublicParams
 func (pp *PublicParams) Bytes() ([]byte, error) {
 	return json.Marshal(pp)
 }
 
+// Serialize marshals a wrapper around PublicParams (SerializedPublicParams)
 func (pp *PublicParams) Serialize() ([]byte, error) {
 	raw, err := json.Marshal(pp)
 	if err != nil {
@@ -73,6 +93,7 @@ func (pp *PublicParams) Serialize() ([]byte, error) {
 	})
 }
 
+// Deserialize un-marshals the passed bytes into PublicParams
 func (pp *PublicParams) Deserialize(raw []byte) error {
 	publicParams := &driver.SerializedPublicParameters{}
 	if err := json.Unmarshal(raw, publicParams); err != nil {
@@ -84,26 +105,33 @@ func (pp *PublicParams) Deserialize(raw []byte) error {
 	return json.Unmarshal(publicParams.Raw, pp)
 }
 
+// AuditorIdentity returns the auditor identity encoded in PublicParams
 func (pp *PublicParams) AuditorIdentity() view.Identity {
 	return pp.Auditor
 }
 
+// AddAuditor sets the Auditor field in PublicParams to the passed identity
 func (pp *PublicParams) AddAuditor(auditor view.Identity) {
 	pp.Auditor = auditor
 }
 
+// AddIssuer adds the passed issuer to the array of Issuers in PublicParams
 func (pp *PublicParams) AddIssuer(issuer view.Identity) {
 	pp.Issuers = append(pp.Issuers, issuer)
 }
 
+// Auditors returns the list of authorized auditors
+// fabtoken only supports a single auditor
 func (pp *PublicParams) Auditors() []view.Identity {
 	return []view.Identity{pp.Auditor}
 }
 
+// Precision returns the quantity precision encoded in PublicParams
 func (pp *PublicParams) Precision() uint64 {
 	return pp.QuantityPrecision
 }
 
+// Setup initializes PublicParams
 func Setup() (*PublicParams, error) {
 	return &PublicParams{
 		MTV:               MaxMoney,
