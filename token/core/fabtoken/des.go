c71bbbaeec8042beafb7342894fe1655d8a6d0be
Kaoutar <kao@zurich.ibm.com>
2022-07-06 15:26:17 +0200

    check deserializer not nil
    Signed-off-by: Kaoutar <kao@zurich.ibm.com>

--- token/core/fabtoken/des.go
+++ token/core/fabtoken/des.go
@@ -8,27 +8,40 @@ package fabtoken
 
 import (
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
+	"github.com/pkg/errors"
 
 	"github.com/hyperledger-labs/fabric-token-sdk/token/driver"
 )
 
 // GetAuditorVerifier deserializes the verifier for the passed auditor identity
 func (s *Service) GetAuditorVerifier(id view.Identity) (driver.Verifier, error) {
+	if s.Deserializer == nil {
+		return nil, errors.New("cannot get auditor verifier: nil deserializer")
+	}
 	return s.Deserializer.GetAuditorVerifier(id)
 }
 
 // GetOwnerVerifier deserializes the verifier for the passed owner identity
 func (s *Service) GetOwnerVerifier(id view.Identity) (driver.Verifier, error) {
+	if s.Deserializer == nil {
+		return nil, errors.New("cannot get owner verifier: nil deserializer")
+	}
 	return s.Deserializer.GetOwnerVerifier(id)
 }
 
 // GetIssuerVerifier deserializes the verifier for the passed issuer identity
 func (s *Service) GetIssuerVerifier(id view.Identity) (driver.Verifier, error) {
+	if s.Deserializer == nil {
+		return nil, errors.New("cannot get issuer verifier: nil deserializer")
+	}
 	return s.Deserializer.GetIssuerVerifier(id)
 }
 
 // GetOwnerMatcher deserializes the passed bytes into a Matcher
 // The Matcher can be used later to match an identity to its audit information
 func (s *Service) GetOwnerMatcher(raw []byte) (driver.Matcher, error) {
+	if s.Deserializer == nil {
+		return nil, errors.New("cannot get owner matcher: nil deserializer")
+	}
 	return s.Deserializer.GetOwnerMatcher(raw)
 }
