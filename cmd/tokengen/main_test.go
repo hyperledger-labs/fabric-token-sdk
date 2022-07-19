a66a1f698e1c262a9e79a87a06553408ad62e463
Angelo De Caro <adc@zurich.ibm.com>
2022-07-19 13:36:23 +0200

    Tokengen should also work without signing key of auditor and issuer #299 (#300)
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- cmd/tokengen/main_test.go
+++ cmd/tokengen/main_test.go
@@ -14,7 +14,10 @@ import (
 	"testing"
 
 	"github.com/hyperledger-labs/fabric-token-sdk/token"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/core/cmd/pp/common"
 	_ "github.com/hyperledger-labs/fabric-token-sdk/token/core/fabtoken/driver"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/core/identity/msp"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/crypto"
 	_ "github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/nogh/driver"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
@@ -27,7 +30,56 @@ func TestCompile(t *testing.T) {
 	defer gexec.CleanupBuildArtifacts()
 }
 
-func TestGen(t *testing.T) {
+func TestGenFullSuccess(t *testing.T) {
+	gt := NewGomegaWithT(t)
+	tokengen, err := gexec.Build("github.com/hyperledger-labs/fabric-token-sdk/cmd/tokengen")
+	gt.Expect(err).NotTo(HaveOccurred())
+	defer gexec.CleanupBuildArtifacts()
+
+	tempOutput, err := ioutil.TempDir("", "tokengen-test")
+	gt.Expect(err).NotTo(HaveOccurred())
+	defer os.RemoveAll(tempOutput)
+
+	testGenRun(
+		gt,
+		tokengen,
+		[]string{
+			"gen",
+			"dlog",
+			"--idemix",
+			"./testdata/idemix",
+			"--issuers",
+			"./testdata/issuers/msp",
+			"--auditors",
+			"./testdata/auditors/msp",
+			"--output",
+			tempOutput,
+		},
+	)
+
+	// Check output
+	ppRaw, err := ioutil.ReadFile(filepath.Join(tempOutput, "zkatdlog_pp.json"))
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	pp, err := crypto.NewPublicParamsFromBytes(ppRaw, crypto.DLogPublicParameters)
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	auditors := pp.Auditors()
+	auditor, err := common.GetMSPIdentity("./testdata/auditors/msp", msp.AuditorMSPID)
+	gt.Expect(err).NotTo(HaveOccurred())
+	gt.Expect(auditors[0]).To(Equal(auditor))
+
+	issuers := pp.Issuers
+	issuer, err := common.GetMSPIdentity("./testdata/issuers/msp", msp.IssuerMSPID)
+	gt.Expect(err).NotTo(HaveOccurred())
+	gt.Expect(issuers[0]).To(BeEquivalentTo(issuer))
+
+	idemixPK, err := ioutil.ReadFile("./testdata/idemix/msp/IssuerPublicKey")
+	gt.Expect(err).NotTo(HaveOccurred())
+	gt.Expect(idemixPK).To(BeEquivalentTo(pp.IdemixPK))
+}
+
+func TestGenFailure(t *testing.T) {
 	gt := NewGomegaWithT(t)
 	tokengen, err := gexec.Build("github.com/hyperledger-labs/fabric-token-sdk/cmd/tokengen")
 	gt.Expect(err).NotTo(HaveOccurred())
@@ -53,7 +105,7 @@ func TestGen(t *testing.T) {
 					driver,
 					"--auditors", "aOrg1MSP,b:Org2MSP",
 				},
-				ErrMsg: "Error: failed to generate public parameters: failed to get auditor identity [aOrg1MSP]: failed to create x509 provider for [aOrg1MSP]: could not load a valid signer certificate from directory aOrg1MSP/signcerts: stat aOrg1MSP/signcerts: no such file or directory",
+				ErrMsg: "Error: failed to generate public parameters: failed to get auditor identity [aOrg1MSP]: failed to load certificates from aOrg1MSP/signcerts: stat aOrg1MSP/signcerts: no such file or directory",
 			},
 			{
 				Args: []string{
@@ -69,7 +121,7 @@ func TestGen(t *testing.T) {
 					driver,
 					"--issuers", "aOrg1MSP,b:Org2MSP",
 				},
-				ErrMsg: "Error: failed to generate public parameters: failed to get issuer identity [aOrg1MSP]: failed to create x509 provider for [aOrg1MSP]: could not load a valid signer certificate from directory aOrg1MSP/signcerts: stat aOrg1MSP/signcerts: no such file or directory",
+				ErrMsg: "Error: failed to generate public parameters: failed to get issuer identity [aOrg1MSP]: failed to load certificates from aOrg1MSP/signcerts: stat aOrg1MSP/signcerts: no such file or directory",
 			},
 		}...,
 		)
