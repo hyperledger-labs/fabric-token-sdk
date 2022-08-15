c4aa4eb1e4c910a1863f1a3a9b0e0366f1118e3e
Alexandros Filios <alexandros.filios@ibm.com>
2022-08-15 10:34:30 +0200

    Cleanup deps.go
    Re-using deps.go from fabric-smart-client and removing redundant deps from blank imports in deps.go
    
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- deps.go
+++ deps.go
@@ -10,18 +10,5 @@ SPDX-License-Identifier: Apache-2.0
 package tools
 
 import (
-	_ "github.com/IBM/idemix/bccsp"
-	_ "github.com/IBM/idemix/bccsp/keystore"
-	_ "github.com/IBM/idemix/bccsp/schemes"
-	_ "github.com/IBM/idemix/bccsp/schemes/dlog/crypto/translator/amcl"
-	_ "github.com/IBM/idemix/tools/idemixgen"
-	_ "github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/tracing"
-	_ "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/services/weaver/relay/fabric"
-	_ "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/comm"
-	_ "github.com/hyperledger/fabric/common/ledger/util/leveldbhelper"
-	_ "github.com/hyperledger/fabric/common/metrics/prometheus"
-	_ "github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb/statecouchdb"
-	_ "github.com/hyperledger/fabric/core/ledger/pvtdatastorage"
-	_ "github.com/hyperledger/fabric/core/operations"
-	_ "github.com/libp2p/go-libp2p-core/network"
+	_ "github.com/hyperledger-labs/fabric-smart-client"
 )
