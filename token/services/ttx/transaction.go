5e84137a74431b57e8aa9df28646e6812f55207b
Angelo De Caro <adc@zurich.ibm.com>
2022-09-18 12:19:55 +0200

    fixup! remove "zkat" magic string #107
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/ttx/transaction.go
+++ token/services/ttx/transaction.go
@@ -9,13 +9,12 @@ package ttx
 import (
 	"encoding/asn1"
 
-	"github.com/hyperledger-labs/fabric-token-sdk/token/services/vault/keys"
-
 	view2 "github.com/hyperledger-labs/fabric-smart-client/platform/view"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/tracker/metrics"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
 	"github.com/hyperledger-labs/fabric-token-sdk/token"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/services/network"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/vault/keys"
 	"github.com/pkg/errors"
 	"go.uber.org/zap/zapcore"
 )
