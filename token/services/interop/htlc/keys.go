f1e049cc300965b0f6805d4d7c804b5f3fe9cd72
Angelo De Caro <adc@zurich.ibm.com>
2022-10-10 11:17:32 +0200

    lock error when same hash is used for lock #392 (#393)
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/interop/htlc/keys.go
+++ token/services/interop/htlc/keys.go
@@ -11,10 +11,21 @@ import (
 )
 
 const (
-	ClaimPreImage = "cpi"
+	ClaimPreImage = "htlc.cpi"
+	LockHash      = "htlc.lh"
 )
 
 // ClaimKey returns the claim key for the passed byte array
 func ClaimKey(v []byte) string {
 	return ClaimPreImage + hex.EncodeToString(v)
+}
+
+// LockKey returns the lock key for the passed byte array
+func LockKey(v []byte) string {
+	return LockHash + hex.EncodeToString(v)
+}
+
+// LockValue returns the encoding of the value for a lock key
+func LockValue(v []byte) []byte {
+	return []byte(hex.EncodeToString(v))
 }
