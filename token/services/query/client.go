2c44aeeaa376fa4a7e47494f897cb8fdf6212815
Hagar Meir <hagar.meir@ibm.com>
2022-11-14 17:08:18 +0200

    deprecate balance queries
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- token/services/query/client.go
+++ token/services/query/client.go
@@ -3,15 +3,15 @@ Copyright IBM Corp. All Rights Reserved.
 
 SPDX-License-Identifier: Apache-2.0
 */
+
 package query
 
 import (
 	"encoding/json"
 
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/common"
-	"github.com/pkg/errors"
-
 	"github.com/hyperledger-labs/fabric-token-sdk/token"
+	"github.com/pkg/errors"
 )
 
 type Client interface {
@@ -26,8 +26,15 @@ func NewClient(vClient Client) *viewClient {
 	return &viewClient{vClient: vClient}
 }
 
-func (c *viewClient) Balance(typ string, opts ...token.ServiceOption) ([]Balance, error) {
-	return c.WalletBalance("", typ, opts...)
+type BalanceQuery struct {
+	TMSID  token.TMSID
+	Wallet string
+	Type   string
+}
+
+type Balance struct {
+	Type     string
+	Quantity string
 }
 
 func (c *viewClient) WalletBalance(wallet, typ string, opts ...token.ServiceOption) ([]Balance, error) {
@@ -36,7 +43,7 @@ func (c *viewClient) WalletBalance(wallet, typ string, opts ...token.ServiceOpti
 		return nil, err
 	}
 
-	balance, err := c.vClient.CallView("zkat.balance.query", common.JSONMarshall(&BalanceQuery{
+	balance, err := c.vClient.CallView("balance", common.JSONMarshall(&BalanceQuery{
 		TMSID: token.TMSID{
 			Network:   options.Network,
 			Channel:   options.Channel,
@@ -54,30 +61,4 @@ func (c *viewClient) WalletBalance(wallet, typ string, opts ...token.ServiceOpti
 		return nil, errors.Errorf("could not retrieve balance")
 	}
 	return []Balance{bal}, nil
-}
-
-func (c *viewClient) AllMyBalances(opts ...token.ServiceOption) ([]Balance, error) {
-	options, err := token.CompileServiceOptions(opts...)
-	if err != nil {
-		return nil, err
-	}
-
-	balances, err := c.vClient.CallView("zkat.all.balance.query",
-		common.JSONMarshall(&AllBalanceQuery{
-			TMSID: token.TMSID{
-				Network:   options.Network,
-				Channel:   options.Channel,
-				Namespace: options.Namespace,
-			},
-		}),
-	)
-	if err != nil {
-		return nil, err
-	}
-	bal := AllMyBalances{}
-	err = json.Unmarshal(balances.([]byte), &bal)
-	if err != nil {
-		return nil, errors.Errorf("could not retrieve balance")
-	}
-	return bal.Balances, err
 }
