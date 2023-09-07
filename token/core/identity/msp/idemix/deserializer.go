71bf8b2b52d7861cc9a65fbdefb92c307376bd52
Angelo De Caro <adc@zurich.ibm.com>
2023-09-07 08:57:29 +0200

    some cleanup
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/identity/msp/idemix/deserializer.go
+++ token/core/identity/msp/idemix/deserializer.go
@@ -7,8 +7,6 @@ SPDX-License-Identifier: Apache-2.0
 package idemix
 
 import (
-	idemix2 "github.com/IBM/idemix/bccsp"
-	"github.com/IBM/idemix/bccsp/keystore"
 	bccsp "github.com/IBM/idemix/bccsp/types"
 	math "github.com/IBM/mathlib"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/msp/idemix"
@@ -19,18 +17,6 @@ import (
 
 type Deserializer struct {
 	*idemix.Deserializer
-}
-
-func (d *Deserializer) DeserializeVerifier(id view.Identity) (driver.Verifier, error) {
-	return d.Deserializer.DeserializeVerifier(id)
-}
-
-func (d *Deserializer) DeserializeVerifierAgainstNymEID(id view.Identity, nymEID []byte) (driver.Verifier, error) {
-	return d.Deserializer.DeserializeVerifierAgainstNymEID(id, nymEID)
-}
-
-func (d *Deserializer) GetOwnerMatcher(raw []byte) (driver.Matcher, error) {
-	return d.Deserializer.DeserializeAuditInfo(raw)
 }
 
 // NewDeserializer returns a new deserializer for the idemix ExpectEidNymRhNym verification strategy
@@ -68,27 +54,14 @@ func NewDeserializerWithProvider(
 	return &Deserializer{Deserializer: d}, nil
 }
 
-// NewKVSBCCSP returns a new BCCSP for the passed curve, if the curve is BLS12_381_BBS, it returns the BCCSP implementation
-// based on aries.
-func NewKVSBCCSP(kvsStore keystore.KVS, curveID math.CurveID) (bccsp.BCCSP, error) {
-	if curveID == math.BLS12_381_BBS {
-		logger.Debugf("new aries KVS-based BCCSP")
-		return idemix.NewKSVBCCSP(kvsStore, curveID, true)
-	}
-	logger.Debugf("new dlog KVS-based BCCSP")
-	return idemix.NewKSVBCCSP(kvsStore, curveID, false)
+func (d *Deserializer) DeserializeVerifier(id view.Identity) (driver.Verifier, error) {
+	return d.Deserializer.DeserializeVerifier(id)
 }
 
-// NewAriesBCCSP returns an instance of the idemix BCCSP for the given curve based on aries
-func NewAriesBCCSP() (bccsp.BCCSP, error) {
-	logger.Infof("new aries no-KeyStore BCCSP")
-	curve, tr, err := idemix.GetCurveAndTranslator(math.BLS12_381_BBS)
-	if err != nil {
-		return nil, err
-	}
-	cryptoProvider, err := idemix2.NewAries(&keystore.Dummy{}, curve, tr, true)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed getting crypto provider")
-	}
-	return cryptoProvider, nil
+func (d *Deserializer) DeserializeVerifierAgainstNymEID(id view.Identity, nymEID []byte) (driver.Verifier, error) {
+	return d.Deserializer.DeserializeVerifierAgainstNymEID(id, nymEID)
+}
+
+func (d *Deserializer) GetOwnerMatcher(raw []byte) (driver.Matcher, error) {
+	return d.Deserializer.DeserializeAuditInfo(raw)
 }
