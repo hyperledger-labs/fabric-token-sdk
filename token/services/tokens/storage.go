e0508c7a4d93bfa9ebe577864ea249b2218b259e
Arne Rutjes <arne123@gmail.com>
2024-03-21 15:08:03 +0100

    add log, move owners to where they are used
    Signed-off-by: Arne Rutjes <arne123@gmail.com>

--- token/services/tokens/storage.go
+++ token/services/tokens/storage.go
@@ -67,10 +67,6 @@ func (t *transaction) DeleteToken(txID string, index uint64, deletedBy string) e
 	if err != nil {
 		return errors.WithMessagef(err, "failed to get token [%s:%d]", txID, index)
 	}
-	owners, err := t.tx.OwnersOf(txID, index)
-	if err != nil {
-		return errors.WithMessagef(err, "failed to get owners for token [%s:%d]", txID, index)
-	}
 	err = t.tx.Delete(txID, index, deletedBy)
 	if err != nil {
 		if tok == nil {
@@ -83,9 +79,13 @@ func (t *transaction) DeleteToken(txID string, index uint64, deletedBy string) e
 		logger.Debugf("nothing further to delete for [%s:%d]", txID, index)
 		return nil
 	}
-	for _, id := range owners {
-		logger.Debugf("post new delete-token event")
-		t.Notify(DeleteToken, t.tmsID, id, tok.Type, txID, index)
+	owners, err := t.tx.OwnersOf(txID, index)
+	if err != nil {
+		return errors.WithMessagef(err, "failed to get owners for token [%s:%d]", txID, index)
+	}
+	for _, owner := range owners {
+		logger.Debugf("post new delete-token event [%s:%s:%s]", txID, index, owner)
+		t.Notify(DeleteToken, t.tmsID, owner, tok.Type, txID, index)
 	}
 	return nil
 }
