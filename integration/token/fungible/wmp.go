4fb35ea65edc53eaab87d8a358a3918a6085b180
Alexandros Filios <alexandros.filios@ibm.com>
2024-03-28 17:43:53 +0100

    fixup! Update FSC dependencies
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/token/fungible/wmp.go
+++ integration/token/fungible/wmp.go
@@ -7,10 +7,10 @@ SPDX-License-Identifier: Apache-2.0
 package fungible
 
 import (
-	"fmt"
 	"path/filepath"
 
 	"github.com/hyperledger-labs/fabric-smart-client/integration"
+	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/node"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/core/config"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/flogging"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/kvs"
@@ -108,7 +108,7 @@ func (l *walletManagerLoader) Load(user string) *token.WalletManager {
 
 	// prepare a service provider with the required services
 	sp := registry.New()
-	configProvider, err := config.NewProvider(filepath.Join(ctx.RootDir(), "fsc", "nodes", fmt.Sprintf("fsc.%s.0", user)))
+	configProvider, err := config.NewProvider(filepath.Join(ctx.RootDir(), "fsc", "nodes", node.ReplicaUniqueName(user, 0)))
 	Expect(err).ToNot(HaveOccurred())
 	Expect(sp.RegisterService(configProvider)).ToNot(HaveOccurred())
 	kvss, err := kvs.NewWithConfig(sp, "memory", "", configProvider)
