0c21f46eb7bbc387a9dbdc6c96ee45a97cc7b19a
Angelo De Caro <adc@zurich.ibm.com>
2024-04-08 12:51:45 +0200

    print commit version, time, and modified flag
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/cmd/version/version.go
+++ token/core/cmd/version/version.go
@@ -9,11 +9,33 @@ package version
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
 const ProgramName = "tokengen"
+
+var Commit, Time, Modified = func() (string, string, string) {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		var revision string
+		var time string
+		var modified string
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				revision = setting.Value
+			}
+			if setting.Key == "vcs.time" {
+				time = setting.Value
+			}
+			if setting.Key == "vcs.modified" {
+				modified = setting.Value
+			}
+		}
+		return revision, time, modified
+	}
+	return "", "", ""
+}()
 
 // Cmd returns the Cobra Command for Version
 func Cmd() *cobra.Command {
@@ -37,8 +59,11 @@ var cobraCommand = &cobra.Command{
 
 // GetInfo returns version information for the peer
 func GetInfo() string {
-	return fmt.Sprintf("%s:\n Go version: %s\n"+
+	return fmt.Sprintf("%s\n"+
+		" Version: (%s, %s, %s)\n "+
+		" Go version: %s\n"+
 		" OS/Arch: %s\n",
-		ProgramName, runtime.Version(),
+		ProgramName, Commit, Time, Modified,
+		runtime.Version(),
 		fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH))
 }
