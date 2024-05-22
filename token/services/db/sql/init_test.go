d99f1a2c79547ad3bb9c051e034c26e33234d5a9
Alexandros Filios <alexandros.filios@ibm.com>
2024-05-22 10:43:13 +0200

    Extracted postgres starter to re-use
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- token/services/db/sql/init_test.go
+++ token/services/db/sql/init_test.go
@@ -7,18 +7,11 @@ SPDX-License-Identifier: Apache-2.0
 package sql
 
 import (
-	"context"
 	"fmt"
-	"os"
-	"runtime/debug"
 	"testing"
-	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/test-go/testify/assert"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
 	_ "modernc.org/sqlite"
 )
 
@@ -73,39 +66,4 @@ func TestGetTableNames(t *testing.T) {
 			assert.Equal(t, tableNames{}, names)
 		})
 	}
-}
-
-// https://testcontainers.com/guides/getting-started-with-testcontainers-for-go/
-// Note: Before running tests: docker pull postgres:16.0-alpine
-// Test may time out if image is not present on machine.
-func startPostgresContainer(t *testing.T) (func(), string) {
-	if os.Getenv("TESTCONTAINERS") != "true" {
-		t.Skip("set environment variable TESTCONTAINERS to true to include postgres test")
-	}
-	if testing.Short() {
-		t.Skip("skipping postgres test in short mode")
-	}
-
-	ctx := context.Background()
-	pg, err := postgres.RunContainer(ctx,
-		testcontainers.WithImage("postgres:16.0-alpine"),
-		testcontainers.WithWaitStrategy(
-			wait.ForExposedPort().WithStartupTimeout(30*time.Second)),
-		postgres.WithDatabase("testdb"),
-		postgres.WithUsername("postgres"),
-		postgres.WithPassword("example"),
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pgConnStr, err := pg.ConnectionString(ctx, "sslmode=disable")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return func() {
-		if err := pg.Terminate(ctx); err != nil {
-			logger.Errorf("failed to terminate [%s][%s]", err, debug.Stack())
-		}
-	}, pgConnStr
 }
