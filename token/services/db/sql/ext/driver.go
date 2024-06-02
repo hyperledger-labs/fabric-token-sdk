cfd270650f4f5bb1ee6878df23743651da2f3087
Angelo De Caro <adc@zurich.ibm.com>
2024-06-02 09:42:02 +0200

    db extensions
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ token/services/db/sql/ext/driver.go
@@ -0,0 +1,17 @@
+package ext
+
+import (
+	"database/sql"
+
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/db/driver"
+)
+
+type Extension interface {
+	GetSchema() string
+}
+
+type TokenDBExtension interface {
+	Extension
+	Delete(tx *sql.Tx, txID string, index uint64, deletedBy string) error
+	StoreToken(tx *sql.Tx, tr driver.TokenRecord, owners []string) error
+}
