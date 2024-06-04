77282466c210d5b2ccaaa17d6b53e18503d301bc
Angelo De Caro <adc@zurich.ibm.com>
2024-06-04 10:21:42 +0200

    fixup! metrics
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/zkatdlog/nogh/metrics.go
+++ token/core/zkatdlog/nogh/metrics.go
@@ -7,16 +7,47 @@ SPDX-License-Identifier: Apache-2.0
 package nogh
 
 import (
+	"time"
+
 	"github.com/hyperledger-labs/fabric-token-sdk/token"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/core/common/metrics"
 )
 
+var (
+	zkIssueDurationOpts = metrics.HistogramOpts{
+		Namespace:    "token_sdk.zkatdlog.nogh",
+		Name:         "issue_duration",
+		Help:         "Duration of zk issue token",
+		LabelNames:   []string{"network", "channel", "namespace"},
+		StatsdFormat: "%{#fqname}.%{network}.%{channel}.%{namespace}",
+	}
+	zkTransferDurationOpts = metrics.HistogramOpts{
+		Namespace:    "token_sdk.zkatdlog.nogh",
+		Name:         "transfer_duration",
+		Help:         "Duration of zk transfer token",
+		LabelNames:   []string{"network", "channel", "namespace"},
+		StatsdFormat: "%{#fqname}.%{network}.%{channel}.%{namespace}",
+	}
+)
+
 type Metrics struct {
 	*metrics.Metrics
+
+	zkIssueDuration    metrics.Histogram
+	zkTransferDuration metrics.Histogram
 }
 
 func NewMetrics(provider metrics.Provider, tmsID token.TMSID) *Metrics {
 	m := &Metrics{Metrics: metrics.New(provider, tmsID)}
-
+	m.zkIssueDuration = m.NewHistogram(zkIssueDurationOpts)
+	m.zkTransferDuration = m.NewHistogram(zkTransferDurationOpts)
 	return m
+}
+
+func (m *Metrics) ObserveZKIssueDuration(duration time.Duration) {
+	m.zkIssueDuration.With().Observe(float64(duration.Milliseconds()))
+}
+
+func (m *Metrics) ObserveZKTransferDuration(duration time.Duration) {
+	m.zkTransferDuration.With().Observe(float64(duration.Milliseconds()))
 }
