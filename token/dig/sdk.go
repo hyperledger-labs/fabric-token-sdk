6680613bbcc02190fbadd5a8f3d6e43d7e1aad58
Alexandros Filios <alexandros.filios@ibm.com>
2024-06-14 09:38:26 +0200

    DIG for tokensdk
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- token/dig/sdk.go
+++ token/dig/sdk.go
@@ -11,9 +11,10 @@ import (
 	errors2 "errors"
 	"time"
 
+	dig2 "github.com/hyperledger-labs/fabric-smart-client/platform/common/sdk/dig"
 	digutils "github.com/hyperledger-labs/fabric-smart-client/platform/common/utils/dig"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core"
-	fabricsdk "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/dig"
+	sdk "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/sdk/dig"
 	view2 "github.com/hyperledger-labs/fabric-smart-client/platform/view"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/driver"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/flogging"
@@ -76,15 +77,15 @@ var selectorProviders = map[string]any{
 }
 
 type SDK struct {
-	*fabricsdk.SDK
+	dig2.SDK
 }
 
 func (p *SDK) TokenEnabled() bool {
-	return p.Config.GetBool("token.enabled")
+	return p.ConfigService().GetBool("token.enabled")
 }
 
 func NewSDK(registry Registry) *SDK {
-	return &SDK{SDK: fabricsdk.NewSDK(registry)}
+	return &SDK{SDK: sdk.NewSDK(registry)}
 }
 
 func (p *SDK) Install() error {
@@ -98,59 +99,59 @@ func (p *SDK) Install() error {
 	logger.Infof("Set TMS TMSProvider")
 
 	err := errors2.Join(
-		p.C.Provide(func(sp view2.ServiceProvider) *network.Provider { return network.NewProvider(sp) }),
-		p.C.Provide(digutils.Identity[*network.Provider](), dig.As(new(ttx.NetworkProvider), new(token.Normalizer), new(auditor.NetworkProvider))),
-		p.C.Provide(func(networkProvider *network.Provider) *vault.PublicParamsProvider {
+		p.Container().Provide(func(sp view2.ServiceProvider) *network.Provider { return network.NewProvider(sp) }),
+		p.Container().Provide(digutils.Identity[*network.Provider](), dig.As(new(ttx.NetworkProvider), new(token.Normalizer), new(auditor.NetworkProvider))),
+		p.Container().Provide(func(networkProvider *network.Provider) *vault.PublicParamsProvider {
 			return &vault.PublicParamsProvider{Provider: networkProvider}
 		}, dig.As(new(core2.Vault))),
-		p.C.Provide(digutils.Identity[driver.ConfigService](), dig.As(new(core.ConfigProvider))),
-		p.C.Provide(func() logging2.Logger { return flogging.MustGetLogger("token-sdk.core") }),
-		p.C.Provide(digutils.Identity[logging2.Logger](), dig.As(new(logging.Logger))),
-		p.C.Provide(core2.NewTMSProvider),
-		p.C.Provide(digutils.Identity[*core2.TMSProvider](), dig.As(new(driver2.TokenManagerServiceProvider))),
-		p.C.Provide(func(service driver.ConfigService) *config2.Service { return config2.NewService(service) }),
-		p.C.Provide(digutils.Identity[*config2.Service](), dig.As(new(core2.ConfigProvider))),
-		p.C.Provide(func(ttxdbManager *ttxdb.Manager) *network2.LockerProvider {
+		p.Container().Provide(digutils.Identity[driver.ConfigService](), dig.As(new(core.ConfigProvider))),
+		p.Container().Provide(func() logging2.Logger { return flogging.MustGetLogger("token-sdk.core") }),
+		p.Container().Provide(digutils.Identity[logging2.Logger](), dig.As(new(logging.Logger))),
+		p.Container().Provide(core2.NewTMSProvider),
+		p.Container().Provide(digutils.Identity[*core2.TMSProvider](), dig.As(new(driver2.TokenManagerServiceProvider))),
+		p.Container().Provide(func(service driver.ConfigService) *config2.Service { return config2.NewService(service) }),
+		p.Container().Provide(digutils.Identity[*config2.Service](), dig.As(new(core2.ConfigProvider))),
+		p.Container().Provide(func(ttxdbManager *ttxdb.Manager) *network2.LockerProvider {
 			return network2.NewLockerProvider(ttxdbManager, 2*time.Second, 5*time.Minute)
 		}),
-		p.C.Provide(selectorProviders[p.Config.GetString("token.selector.driver")], dig.As(new(token.SelectorManagerProvider))),
-		p.C.Provide(network2.NewCertificationClientProvider, dig.As(new(token.CertificationClientProvider))),
-		p.C.Provide(func(networkProvider *network.Provider) *vault.ProviderAdaptor {
+		p.Container().Provide(selectorProviders[p.ConfigService().GetString("token.selector.driver")], dig.As(new(token.SelectorManagerProvider))),
+		p.Container().Provide(network2.NewCertificationClientProvider, dig.As(new(token.CertificationClientProvider))),
+		p.Container().Provide(func(networkProvider *network.Provider) *vault.ProviderAdaptor {
 			return &vault.ProviderAdaptor{Provider: networkProvider}
 		}, dig.As(new(token.VaultProvider))),
-		p.C.Provide(token.NewManagementServiceProvider),
-		p.C.Provide(digutils.Identity[*token.ManagementServiceProvider](), dig.As(new(ttx.TMSProvider), new(tokens.TMSProvider), new(auditor.TokenManagementServiceProvider))),
-		p.C.Provide(func(configService driver.ConfigService, configProvider *config2.Service) *ttxdb.Manager {
+		p.Container().Provide(token.NewManagementServiceProvider),
+		p.Container().Provide(digutils.Identity[*token.ManagementServiceProvider](), dig.As(new(ttx.TMSProvider), new(tokens.TMSProvider), new(auditor.TokenManagementServiceProvider))),
+		p.Container().Provide(func(configService driver.ConfigService, configProvider *config2.Service) *ttxdb.Manager {
 			return ttxdb.NewManager(configService, dbconfig.NewConfig(configProvider, "ttxdb.persistence.type", "db.persistence.type"))
 		}),
-		p.C.Provide(digutils.Identity[*ttxdb.Manager](), dig.As(new(ttx.DBProvider), new(network2.TTXDBProvider))),
-		p.C.Provide(func(configService driver.ConfigService, configProvider *config2.Service) *tokendb.Manager {
+		p.Container().Provide(digutils.Identity[*ttxdb.Manager](), dig.As(new(ttx.DBProvider), new(network2.TTXDBProvider))),
+		p.Container().Provide(func(configService driver.ConfigService, configProvider *config2.Service) *tokendb.Manager {
 			return tokendb.NewManager(configService, dbconfig.NewConfig(configProvider, "tokendb.persistence.type", "db.persistence.type"))
 		}),
-		p.C.Provide(digutils.Identity[*tokendb.Manager](), dig.As(new(tokens.DBProvider))),
-		p.C.Provide(func(configService driver.ConfigService, configProvider *config2.Service) *auditdb.Manager {
+		p.Container().Provide(digutils.Identity[*tokendb.Manager](), dig.As(new(tokens.DBProvider))),
+		p.Container().Provide(func(configService driver.ConfigService, configProvider *config2.Service) *auditdb.Manager {
 			return auditdb.NewManager(configService, dbconfig.NewConfig(configProvider, "ttxdb.persistence.type", "db.persistence.type"))
 		}),
-		p.C.Provide(digutils.Identity[*auditdb.Manager](), dig.As(new(auditor.AuditDBProvider))),
-		p.C.Provide(func(configService driver.ConfigService, configProvider *config2.Service) *identitydb.Manager {
+		p.Container().Provide(digutils.Identity[*auditdb.Manager](), dig.As(new(auditor.AuditDBProvider))),
+		p.Container().Provide(func(configService driver.ConfigService, configProvider *config2.Service) *identitydb.Manager {
 			return identitydb.NewManager(configService, dbconfig.NewConfig(configProvider, "ttxdb.persistence.type", "db.persistence.type"))
 		}),
-		p.C.Provide(func(configService driver.ConfigService, configProvider *config2.Service) *tokenlockdb.Manager {
+		p.Container().Provide(func(configService driver.ConfigService, configProvider *config2.Service) *tokenlockdb.Manager {
 			return tokenlockdb.NewManager(configService, dbconfig.NewConfig(configProvider, "tokenlockdb.persistence.type", "db.persistence.type"))
 		}),
-		p.C.Provide(digutils.Identity[*kvs.KVS](), dig.As(new(kvs2.KVS))),
-		p.C.Provide(identity.NewDBStorageProvider),
-		p.C.Provide(auditor.NewManager),
-		p.C.Provide(ttx.NewManager),
-		p.C.Provide(func() *tokens2.AuthorizationMultiplexer {
+		p.Container().Provide(digutils.Identity[*kvs.KVS](), dig.As(new(kvs2.KVS))),
+		p.Container().Provide(identity.NewDBStorageProvider),
+		p.Container().Provide(auditor.NewManager),
+		p.Container().Provide(ttx.NewManager),
+		p.Container().Provide(func() *tokens2.AuthorizationMultiplexer {
 			return tokens2.NewAuthorizationMultiplexer(&tokens2.TMSAuthorization{}, &htlc.ScriptOwnership{})
 		}, dig.As(new(tokens.Authorization))),
-		p.C.Provide(func() *tokens2.IssuedMultiplexer { return tokens2.NewIssuedMultiplexer(&tokens2.WalletIssued{}) }, dig.As(new(tokens.Issued))),
-		p.C.Provide(tokens.NewManager),
-		p.C.Provide(digutils.Identity[*tokens.Manager](), dig.As(new(ttx.TokensProvider), new(auditor.TokenDBProvider))),
-		p.C.Provide(vault.NewVaultProvider),
-		p.C.Provide(tms.NewPostInitializer),
-		p.C.Provide(ttx.NewMetrics),
+		p.Container().Provide(func() *tokens2.IssuedMultiplexer { return tokens2.NewIssuedMultiplexer(&tokens2.WalletIssued{}) }, dig.As(new(tokens.Issued))),
+		p.Container().Provide(tokens.NewManager),
+		p.Container().Provide(digutils.Identity[*tokens.Manager](), dig.As(new(ttx.TokensProvider), new(auditor.TokenDBProvider))),
+		p.Container().Provide(vault.NewVaultProvider),
+		p.Container().Provide(tms.NewPostInitializer),
+		p.Container().Provide(ttx.NewMetrics),
 	)
 
 	if err != nil {
@@ -163,28 +164,28 @@ func (p *SDK) Install() error {
 
 	// Backward compatibility with SP
 	err = errors2.Join(
-		digutils.Register[*network.Provider](p.C),
-		digutils.Register[*token.ManagementServiceProvider](p.C),
-		digutils.Register[*ttxdb.Manager](p.C),
-		digutils.Register[*tokendb.Manager](p.C),
-		digutils.Register[*auditdb.Manager](p.C),
-		digutils.Register[*identitydb.Manager](p.C),
-		digutils.Register[*vault.Provider](p.C),
-		digutils.Register[driver.ConfigService](p.C),
-		digutils.Register[*identity.DBStorageProvider](p.C),
-		digutils.Register[*ttx.Metrics](p.C),
-		digutils.Register[*auditor.Manager](p.C),
-		digutils.Register[*config2.Service](p.C),
-		digutils.Register[*ttx.Manager](p.C),
-		digutils.Register[*tokens.Manager](p.C),
-		digutils.Register[*tracing.Provider](p.C),
+		digutils.Register[*network.Provider](p.Container()),
+		digutils.Register[*token.ManagementServiceProvider](p.Container()),
+		digutils.Register[*ttxdb.Manager](p.Container()),
+		digutils.Register[*tokendb.Manager](p.Container()),
+		digutils.Register[*auditdb.Manager](p.Container()),
+		digutils.Register[*identitydb.Manager](p.Container()),
+		digutils.Register[*vault.Provider](p.Container()),
+		digutils.Register[driver.ConfigService](p.Container()),
+		digutils.Register[*identity.DBStorageProvider](p.Container()),
+		digutils.Register[*ttx.Metrics](p.Container()),
+		digutils.Register[*auditor.Manager](p.Container()),
+		digutils.Register[*config2.Service](p.Container()),
+		digutils.Register[*ttx.Manager](p.Container()),
+		digutils.Register[*tokens.Manager](p.Container()),
+		digutils.Register[*tracing.Provider](p.Container()),
 	)
 	if err != nil {
 		return err
 	}
 
 	return errors2.Join(
-		p.C.Invoke(func(tmsProvider *core2.TMSProvider, postInitializer *tms.PostInitializer) {
+		p.Container().Invoke(func(tmsProvider *core2.TMSProvider, postInitializer *tms.PostInitializer) {
 			tmsProvider.SetCallback(postInitializer.PostInit)
 		}),
 	)
@@ -200,7 +201,7 @@ func (p *SDK) Start(ctx context.Context) error {
 	}
 	logger.Infof("Token platform enabled, starting...")
 
-	return p.C.Invoke(func(configService *config2.Service, networkProvider *network.Provider, tmsProvider *token.ManagementServiceProvider) error {
+	return p.Container().Invoke(func(configService *config2.Service, networkProvider *network.Provider, tmsProvider *token.ManagementServiceProvider) error {
 		configurations, err := configService.Configurations()
 		if err != nil {
 			return err
