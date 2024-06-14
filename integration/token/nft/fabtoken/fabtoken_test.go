008b36ba8b93e1bc135c1cefc68ef62637f7d87c
Alexandros Filios <alexandros.filios@ibm.com>
2024-06-14 10:14:37 +0200

    DIG for tokensdk
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/token/nft/fabtoken/fabtoken_test.go
+++ integration/token/nft/fabtoken/fabtoken_test.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package fabtoken
 
 import (
+	integration2 "github.com/hyperledger-labs/fabric-smart-client/integration"
 	"github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc"
 	"github.com/hyperledger-labs/fabric-smart-client/pkg/api"
 	"github.com/hyperledger-labs/fabric-token-sdk/integration"
@@ -30,12 +31,12 @@ var _ = Describe("EndToEnd", func() {
 
 func newTestSuite(commType fsc.P2PCommunicationType, factor int, names ...string) (*token.TestSuite, *token.ReplicaSelector) {
 	opts, selector := token.NewReplicationOptions(factor, names...)
-	ts := token.NewTestSuite(opts.SQLConfigs, StartPortDlog, nft.Topology(common.Opts{
+	ts := token.NewTestSuite(opts.SQLConfigs, StartPortDlog, integration2.ReplaceTemplate(nft.Topology(common.Opts{
 		Backend:         "fabric",
 		CommType:        commType,
 		TokenSDKDriver:  "fabtoken",
 		SDKs:            []api.SDK{&ffabtoken.SDK{}},
 		ReplicationOpts: opts,
-	}))
+	})))
 	return ts, selector
 }
