e0799c59a836b25173a842a699b5e0c95b42c38e
Alexandros Filios <alexandros.filios@ibm.com>
2024-06-19 16:29:51 +0200

    fixup! Added tracer implementation for observable services
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- token/core/common/tracing/provider.go
+++ token/core/common/tracing/provider.go
@@ -7,12 +7,9 @@ SPDX-License-Identifier: Apache-2.0
 package tracing
 
 import (
-	"fmt"
-
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/sdk/tracing"
 	metrics2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/metrics"
-	"github.com/hyperledger-labs/fabric-token-sdk/token/core/common/metrics"
 	"go.opentelemetry.io/otel/trace"
-	"go.opentelemetry.io/otel/trace/embedded"
 )
 
 type metricsProvider interface {
@@ -20,44 +17,8 @@ type metricsProvider interface {
 	NewHistogram(opts metrics2.HistogramOpts) metrics2.Histogram
 }
 
-func NewProvider(metricsProvider metricsProvider) trace.TracerProvider {
-	//lint:ignore SA1019 This implementation is faster than the current one, and we don't need to copy the code
-	return NewProviderWithBackingProvider(metricsProvider, trace.NewNoopTracerProvider())
-}
+func NewProvider(metricsProvider) trace.TracerProvider {
 
-func NewProviderWithBackingProvider(metricsProvider metricsProvider, backingProvider trace.TracerProvider) trace.TracerProvider {
-	return &tracerProvider{
-		metricsProvider: metricsProvider,
-		backingProvider: backingProvider,
-	}
-}
-
-type tracerProvider struct {
-	embedded.TracerProvider
-
-	metricsProvider metricsProvider
-	backingProvider trace.TracerProvider
-}
-
-func (p *tracerProvider) Tracer(name string, options ...trace.TracerOption) trace.Tracer {
-	c := trace.NewTracerConfig(options...)
-
-	opts := extractMetricsOpts(c.InstrumentationAttributes())
-	return &tracer{
-		backingTracer: p.backingProvider.Tracer(name, options...),
-		operations: p.metricsProvider.NewCounter(metrics.CounterOpts{
-			Namespace:    opts.Namespace,
-			Name:         fmt.Sprintf("%s_operations", name),
-			Help:         fmt.Sprintf("Counter of '%s' operations", name),
-			LabelNames:   opts.LabelNames,
-			StatsdFormat: opts.StatsdFormat,
-		}),
-		duration: p.metricsProvider.NewHistogram(metrics.HistogramOpts{
-			Namespace:    opts.Namespace,
-			Name:         fmt.Sprintf("%s_duration", name),
-			Help:         fmt.Sprintf("Histogram for the duration of '%s' operations", name),
-			LabelNames:   opts.LabelNames,
-			StatsdFormat: opts.StatsdFormat,
-		}),
-	}
+	p, _ := tracing.NoopProvider()
+	return p
 }
