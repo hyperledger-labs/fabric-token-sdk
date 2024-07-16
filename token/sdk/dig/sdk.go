4f4d347f14cef695457a15d4cac5b069a1b54265
Alexandros Filios <alexandros.filios@ibm.com>
2024-07-16 20:27:56 +0200

    Disabled tests
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- token/sdk/dig/sdk.go
+++ token/sdk/dig/sdk.go
@@ -22,7 +22,6 @@ import (
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/flogging"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/kvs"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/metrics/operations"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/metrics/prometheus"
 	tracing3 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/tracing"
 	"github.com/hyperledger-labs/fabric-token-sdk/token"
 	core2 "github.com/hyperledger-labs/fabric-token-sdk/token/core"
@@ -145,9 +144,6 @@ func (p *SDK) Install() error {
 
 	// Overwrite dependencies
 	err = errors2.Join(
-		p.Container().Decorate(func(_ *operations.System, s operations.Server, o *operations.Options, l operations.OperationsLogger) *operations.System {
-			return operations.NewOperationSystem(s, l, &prometheus.Provider{}, o)
-		}),
 		p.Container().Decorate(func(p metrics.Provider) metrics.Provider {
 			return operations.NewDisabledHistogram(p)
 		}),
