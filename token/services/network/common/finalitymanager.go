d0586296c3fe67180f2f5fb390c2d32f8057e26d
Alexandros Filios <alexandros.filios@ibm.com>
2024-07-23 15:17:43 +0200

    Added parallel finality listener manager
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

---
+++ token/services/network/common/finalitymanager.go
@@ -0,0 +1,31 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package common
+
+import (
+	"github.com/hyperledger-labs/fabric-smart-client/platform/common/driver"
+)
+
+type parallelListenerManagerProvider[V comparable] struct {
+	provider driver.ListenerManagerProvider[V]
+}
+
+func NewParallelListenerManagerProvider[V comparable](provider driver.ListenerManagerProvider[V]) *parallelListenerManagerProvider[V] {
+	return &parallelListenerManagerProvider[V]{provider: provider}
+}
+
+func (p *parallelListenerManagerProvider[V]) NewManager() driver.ListenerManager[V] {
+	return &parallelListenerManager[V]{ListenerManager: p.provider.NewManager()}
+}
+
+type parallelListenerManager[V comparable] struct {
+	driver.ListenerManager[V]
+}
+
+func (m *parallelListenerManager[V]) InvokeListeners(event driver.FinalityEvent[V]) {
+	go m.ListenerManager.InvokeListeners(event)
+}
