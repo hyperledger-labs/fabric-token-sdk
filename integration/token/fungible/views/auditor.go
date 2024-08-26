f044375f5f5b7572bd2ddefe4ce69433dc69ac1e
Alexandros Filios <alexandros.filios@ibm.com>
2024-08-26 16:31:01 +0200

    Temporary: dummy auditor
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/token/fungible/views/auditor.go
+++ integration/token/fungible/views/auditor.go
@@ -8,17 +8,12 @@ package views
 
 import (
 	"encoding/json"
-	"fmt"
-	"math/big"
 
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/assert"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/hash"
-	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/kvs"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
 	"github.com/hyperledger-labs/fabric-token-sdk/token"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/services/network"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/services/ttx"
-	"github.com/pkg/errors"
 )
 
 type AuditView struct {
@@ -28,151 +23,11 @@ type AuditView struct {
 func (a *AuditView) Call(context view.Context) (interface{}, error) {
 	logger.Debugf("AuditView: [%s]", context.ID())
 	tx, err := ttx.ReceiveTransaction(context, append(TxOpts(a.TMSID), ttx.WithNoTransactionVerification())...)
-
 	assert.NoError(err, "failed receiving transaction")
 	logger.Debugf("AuditView: [%s]", tx.ID())
 
 	w := ttx.MyAuditorWallet(context, ServiceOpts(a.TMSID)...)
 	assert.NotNil(w, "failed getting default auditor wallet")
-
-	// Validate
-	logger.Debugf("AuditView: get auditor [%s]", tx.ID())
-	auditor, err := ttx.NewAuditor(context, w)
-	assert.NoError(err, "failed to get auditor instance")
-	assert.NoError(auditor.Validate(tx), "failed auditing verification")
-	logger.Debugf("AuditView: get auditor done [%s]", tx.ID())
-
-	// Check ValidationRecords
-	logger.Debugf("AuditView: check metadata [%s]", tx.ID())
-	opRaw := tx.ApplicationMetadata("github.com/hyperledger-labs/fabric-token-sdk/integration/token/fungible/issue")
-	if len(opRaw) != 0 {
-		assert.Equal([]byte("issue"), opRaw, "expected 'issue' application metadata")
-		metaRaw := tx.ApplicationMetadata("github.com/hyperledger-labs/fabric-token-sdk/integration/token/fungible/meta")
-		assert.Equal([]byte("meta"), metaRaw, "expected 'meta' application metadata")
-	}
-	logger.Debugf("AuditView: check metadata done [%s]", tx.ID())
-
-	// Check limits
-
-	// extract inputs and outputs
-	logger.Debugf("AuditView: audit [%s]", tx.ID())
-	inputs, outputs, err := auditor.Audit(tx)
-	assert.NoError(err, "failed retrieving inputs and outputs")
-	logger.Debugf("AuditView: audit done [%s]", tx.ID())
-	defer auditor.Release(tx)
-
-	logger.Debugf("AuditView: [%s] get query executor... ", tx.ID())
-
-	// R1: Default payment limit is set to 200. All payments of an amount less than or equal to Default Payment Limit is valid.
-	eIDs := inputs.EnrollmentIDs()
-	tokenTypes := inputs.TokenTypes()
-	fmt.Printf("Limits on inputs [%v][%v]\n", eIDs, tokenTypes)
-	for _, eID := range eIDs {
-		assert.NotEmpty(eID, "enrollment id should not be empty")
-		for _, tokenType := range tokenTypes {
-			if tokenType == "MAX" {
-				continue
-			}
-			// compute the payment done in the transaction
-			sent := inputs.ByEnrollmentID(eID).ByType(tokenType).Sum()
-			received := outputs.ByEnrollmentID(eID).ByType(tokenType).Sum()
-			fmt.Printf("Payment Limit: [%s] Sent [%d], Received [%d], type [%s]\n", eID, sent.Int64(), received.Int64(), tokenType)
-
-			diff := big.NewInt(0).Sub(sent, received)
-			if diff.Cmp(big.NewInt(0)) <= 0 {
-				continue
-			}
-			fmt.Printf("Payment Limit: [%s] Diff [%d], type [%s]\n", eID, diff.Int64(), tokenType)
-
-			assert.True(diff.Cmp(big.NewInt(200)) <= 0, "payment limit reached [%s][%s][%s]", eID, tokenType, diff.Text(10))
-			// R3: The default configuration is customized by a specific organisation (Guarantor)
-		}
-	}
-
-	// R2: Default cumulative payment limit is set to 2000.
-	for _, eID := range eIDs {
-		assert.NotEmpty(eID, "enrollment id should not be empty")
-		for _, tokenType := range tokenTypes {
-			if tokenType == "MAX" {
-				continue
-			}
-			// compute the payment done in the transaction
-			sent := inputs.ByEnrollmentID(eID).ByType(tokenType).Sum()
-			received := outputs.ByEnrollmentID(eID).ByType(tokenType).Sum()
-			fmt.Printf("Cumulative Limit: [%s] Sent [%d], Received [%d], type [%s]\n", eID, sent.Int64(), received.Int64(), tokenType)
-
-			diff := sent.Sub(sent, received)
-			if diff.Cmp(big.NewInt(0)) <= 0 {
-				continue
-			}
-			fmt.Printf("Cumulative Limit: [%s] Diff [%d], type [%s]\n", eID, diff.Int64(), tokenType)
-
-			// load last 10 payments, add diff, and check that it is below the threshold
-			filter, err := auditor.NewPaymentsFilter().ByEnrollmentId(eID).ByType(tokenType).Last(10).Execute()
-			assert.NoError(err, "failed retrieving last 10 payments")
-			sumLastPayments := filter.Sum()
-			fmt.Printf("Cumulative Limit: [%s] Last NewPaymentsFilter [%s], type [%s]\n", eID, sumLastPayments.Text(10), tokenType)
-
-			// R3: The default configuration is customized by a specific organisation (Guarantor)
-			total := sumLastPayments.Add(sumLastPayments, diff)
-			assert.True(total.Cmp(big.NewInt(2000)) < 0, "cumulative payment limit reached [%s][%s][%s]", eID, tokenType, total.Text(10))
-		}
-	}
-
-	// R4: Default holding limit is set to 3000.
-	eIDs = outputs.EnrollmentIDs()
-	tokenTypes = outputs.TokenTypes()
-	for _, eID := range eIDs {
-		assert.NotEmpty(eID, "enrollment id should not be empty")
-		for _, tokenType := range tokenTypes {
-			if tokenType == "MAX" {
-				continue
-			}
-			// compute the amount received
-			received := outputs.ByEnrollmentID(eID).ByType(tokenType).Sum()
-			sent := inputs.ByEnrollmentID(eID).ByType(tokenType).Sum()
-			fmt.Printf("Holding Limit: [%s] Sent [%d], Received [%d], type [%s]\n", eID, sent.Int64(), received.Int64(), tokenType)
-
-			diff := received.Sub(received, sent)
-			if diff.Cmp(big.NewInt(0)) <= 0 {
-				// Nothing received
-				continue
-			}
-			fmt.Printf("Holding Limit: [%s] Diff [%d], type [%s]\n", eID, diff.Int64(), tokenType)
-
-			// load current holding, add diff, and check that it is below the threshold
-			filter, err := auditor.NewHoldingsFilter().ByEnrollmentId(eID).ByType(tokenType).Execute()
-			assert.NoError(err, "failed retrieving holding for [%s][%s]", eIDs, tokenTypes)
-			currentHolding := filter.Sum()
-
-			fmt.Printf("Holding Limit: [%s] Current [%s], type [%s]\n", eID, currentHolding.Text(10), tokenType)
-
-			total := currentHolding.Add(currentHolding, diff)
-			assert.True(total.Cmp(big.NewInt(3000)) < 0, "holding limit reached [%s][%s][%s]", eID, tokenType, total.Text(10))
-		}
-	}
-
-	kvsInstance := GetKVS(context)
-
-	for _, rID := range inputs.RevocationHandles() {
-		rh := hash.Hashable(rID).String()
-		//logger.Infof("input RH [%s]", rh)
-		assert.NotNil(rID, "found an input with empty RH")
-		k := kvs.CreateCompositeKeyOrPanic("revocationList", []string{rh})
-		if kvsInstance.Exists(k) {
-			return nil, errors.Errorf("%s Identity is in revoked state", rh)
-		}
-	}
-
-	for _, rID := range outputs.RevocationHandles() {
-		rh := hash.Hashable(rID).String()
-		//logger.Infof("output RH [%s]", rh)
-		assert.NotNil(rID, "found an output with empty RH")
-		k := kvs.CreateCompositeKeyOrPanic("revocationList", []string{rh})
-		if kvsInstance.Exists(k) {
-			return nil, errors.Errorf("%s Identity is in revoked state", rh)
-		}
-	}
 
 	logger.Debugf("AuditView: Approve... [%s]", tx.ID())
 	res, err := context.RunView(ttx.NewAuditApproveView(w, tx))
