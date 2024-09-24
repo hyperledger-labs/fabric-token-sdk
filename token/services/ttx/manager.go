771e67368c32662240cf2e4380bc6ab9899ae57a
Arne Rutjes <arne123@gmail.com>
2024-09-24 10:15:50 +0200

    also attempt to restore transactions marked as deleted
    Signed-off-by: Arne Rutjes <arne123@gmail.com>

--- token/services/ttx/manager.go
+++ token/services/ttx/manager.go
@@ -119,7 +119,7 @@ func (m *Manager) RestoreTMS(tmsID token.TMSID) error {
 		return errors.WithMessagef(err, "failed to get db for [%s:%s]", tmsID.Network, tmsID.Channel)
 	}
 
-	it, err := db.ttxDB.TokenRequests(ttxdb.QueryTokenRequestsParams{Statuses: []TxStatus{driver.Pending, driver.Unknown}})
+	it, err := db.ttxDB.TokenRequests(ttxdb.QueryTokenRequestsParams{Statuses: []TxStatus{driver.Pending, driver.Unknown, driver.Deleted}})
 	if err != nil {
 		return errors.WithMessagef(err, "failed to get tx iterator for [%s:%s:%s]", tmsID.Network, tmsID.Channel, tmsID)
 	}
