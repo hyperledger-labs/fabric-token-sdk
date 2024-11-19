f70aefb9f0547e0b01a438bfc232211665ba74ec
Angelo De Caro <adc@zurich.ibm.com>
2024-11-19 10:12:16 +0100

    checks on setup and tokens service
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/zkatdlog/nogh/tokens.go
+++ token/core/zkatdlog/nogh/tokens.go
@@ -9,6 +9,7 @@ package nogh
 import (
 	"github.com/hyperledger-labs/fabric-token-sdk/token/core/common"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/crypto"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/crypto/math"
 	token2 "github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/crypto/token"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/driver"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/token"
@@ -27,16 +28,16 @@ func NewTokensService(publicParametersManager common.PublicParametersManager[*cr
 // DeserializeToken un-marshals a token and token info from raw bytes
 // It checks if the un-marshalled token matches the token info. If not, it returns
 // an error. Else it returns the token in cleartext and the identity of its issuer
-func (s *TokensService) DeserializeToken(tok []byte, infoRaw []byte) (*token.Token, driver.Identity, error) {
+func (s *TokensService) DeserializeToken(outputRaw []byte, metadataRaw []byte) (*token.Token, driver.Identity, error) {
 	// get zkatdlog token
-	output := &token2.Token{}
-	if err := output.Deserialize(tok); err != nil {
-		return nil, nil, errors.Wrap(err, "failed to deserialize zkatdlog token")
+	output, err := s.getOutput(outputRaw)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "failed getting token output")
 	}
 
 	// get token info
 	ti := &token2.Metadata{}
-	err := ti.Deserialize(infoRaw)
+	err = ti.Deserialize(metadataRaw)
 	if err != nil {
 		return nil, nil, errors.Wrap(err, "failed to deserialize token information")
 	}
@@ -49,6 +50,21 @@ func (s *TokensService) DeserializeToken(tok []byte, infoRaw []byte) (*token.Tok
 	return to, ti.Issuer, nil
 }
 
-func (s *TokensService) IsSpendable(output []byte, outputMetadata []byte) error {
-	return nil
+func (s *TokensService) IsSpendable(outputRaw []byte, metadataRaw []byte) error {
+	_, _, err := s.DeserializeToken(outputRaw, metadataRaw)
+	return err
+}
+
+func (s *TokensService) getOutput(outputRaw []byte) (*token2.Token, error) {
+	output := &token2.Token{}
+	if err := output.Deserialize(outputRaw); err != nil {
+		return nil, errors.Wrap(err, "failed to deserialize oken")
+	}
+	if len(output.Owner) == 0 {
+		return nil, errors.Errorf("token owner not found in output")
+	}
+	if err := math.CheckElement(output.Data, s.PublicParametersManager.PublicParams().Curve); err != nil {
+		return nil, errors.Wrap(err, "data in invalid in output")
+	}
+	return output, nil
 }
