3ccd975c8bc1ee810b98b507f738312d4ca1bc74
Alexandros Filios <alexandros.filios@ibm.com>
2024-12-31 17:02:57 +0200

    Configurable finality config
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- token/services/network/fabric/driver.go
+++ token/services/network/fabric/driver.go
@@ -8,7 +8,6 @@ package fabric
 
 import (
 	"slices"
-	"time"
 
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric"
 	config2 "github.com/hyperledger-labs/fabric-smart-client/platform/fabric/core/generic/config"
@@ -36,9 +35,9 @@ var listenerManagerConfig = finality.ListenerManagerConfig{
 	//CommitterRetryWaitDuration: 5 * time.Second,
 
 	DeliveryMapperParallelism: 10,
-	DeliveryListenerTimeout:   10 * time.Second,
-	DeliveryLRUSize:           100,
-	DeliveryLRUBuffer:         50,
+	//DeliveryListenerTimeout:   10 * time.Second,
+	//DeliveryLRUSize:           100,
+	//DeliveryLRUBuffer:         50,
 }
 
 type Driver struct {
