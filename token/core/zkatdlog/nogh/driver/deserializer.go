438ae1f5e96756a2dbee55d32e4a432b91e01159
Angelo De Caro <adc@zurich.ibm.com>
2025-01-16 12:52:59 +0100

    reuse validator extended deserializer
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/zkatdlog/nogh/driver/deserializer.go
+++ token/core/zkatdlog/nogh/driver/deserializer.go
@@ -29,6 +29,7 @@ func NewDeserializer(pp *crypto.PublicParams) (*Deserializer, error) {
 	if pp == nil {
 		return nil, errors.New("failed to get deserializer: nil public parameters")
 	}
+
 	idemixDes, err := idemix.NewDeserializer(pp.IdemixIssuerPublicKeys[0].PublicKey, pp.IdemixIssuerPublicKeys[0].Curve)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed getting idemix deserializer for passed public params [%d]", pp.IdemixIssuerPublicKeys[0].Curve)
