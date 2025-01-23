5b8f25a6a0d5a8032f9ba0319047a5b6e4faf2a3
Alexandros Filios <alexandros.filios@ibm.com>
2025-01-23 11:57:10 +0100

    fixup! Update FSC version
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- integration/nwo/token/options.go
+++ integration/nwo/token/options.go
@@ -8,9 +8,7 @@ package token
 
 import (
 	fsc "github.com/hyperledger-labs/fabric-smart-client/integration/nwo/fsc/node"
-	sql2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/db/driver/sql"
 	"github.com/hyperledger-labs/fabric-token-sdk/integration/nwo/token/topology"
-	"github.com/hyperledger-labs/fabric-token-sdk/token/services/db/sql/common"
 )
 
 func WithIssuerIdentity(label string, hsm bool) fsc.Option {
@@ -36,21 +34,6 @@ func WithIssuerIdentityWithHSM(label string) fsc.Option {
 
 		if label != "_default_" {
 			o.AddAlias(label)
-		}
-		return nil
-	}
-}
-
-func WithPostgresPersistence(config common.DataSourceProvider) fsc.Option {
-	return func(o *fsc.Options) error {
-		if config != nil {
-			o.PutPersistence("token", fsc.PersistenceOpts{
-				Type: sql2.SQLPersistence,
-				SQL: fsc.SQLOpts{
-					DataSource: config.DataSource(),
-					DriverType: sql2.Postgres,
-				},
-			})
 		}
 		return nil
 	}
