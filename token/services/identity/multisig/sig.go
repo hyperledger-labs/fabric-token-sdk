af19b1ea2987155d56cca8d87de848d732f0b527
kaoutar <kao@zurich.ibm.com>
2025-01-29 17:24:05 +0100

    first drop
    Signed-off-by: kaoutar <kao@zurich.ibm.com>

---
+++ token/services/identity/multisig/sig.go
@@ -0,0 +1,60 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package multisig
+
+import (
+	"encoding/asn1"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/driver"
+	"github.com/pkg/errors"
+)
+
+type MultiIdentity struct {
+	Identities []driver.Identity
+}
+
+type MultiVerifier struct {
+	Verifiers []driver.Verifier
+}
+
+type MultiSignature struct {
+	Signatures [][]byte
+}
+
+func (v *MultiVerifier) Verify(msg, raw []byte) error {
+	sig := &MultiSignature{}
+	_, err := asn1.Unmarshal(raw, sig)
+	if err != nil {
+		return errors.Wrapf(err, "failed to unmarshal multisig")
+	}
+	if len(v.Verifiers) != len(sig.Signatures) {
+		return errors.Errorf("Invalid multisig:  expect % d signatures, "+
+			"but received % d", len(v.Verifiers), len(sig.Signatures))
+	}
+	for k, ver := range v.Verifiers {
+		if err = ver.Verify(msg, sig.Signatures[k]); err != nil {
+			return errors.Errorf("Invalid multisig: signature at index "+
+				"%d does not verify", k)
+		}
+	}
+	return nil
+}
+
+func (id *MultiIdentity) Serialize() ([]byte, error) {
+	raw, err := asn1.Marshal(id)
+	if err != nil {
+		return nil, err
+	}
+	return raw, nil
+}
+
+func (id *MultiIdentity) Deserialize(raw []byte) error {
+	_, err := asn1.Unmarshal(raw, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
