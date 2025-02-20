1f8cc500612d932bc45e72e80964330ac186464c
Angelo De Caro <adc@zurich.ibm.com>
2025-02-20 15:38:25 +0100

    start the finality and lookup service from the latest block
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/gobuffalo/packr/v2 v2.7.1
 	github.com/hashicorp/go-uuid v1.0.3
-	github.com/hyperledger-labs/fabric-smart-client v0.4.1-0.20250217145249-aeb784b94851
+	github.com/hyperledger-labs/fabric-smart-client v0.4.1-0.20250219102202-bcd92755c1b4
 	github.com/hyperledger-labs/orion-sdk-go v0.2.10
 	github.com/hyperledger-labs/orion-server v0.2.10
 	github.com/hyperledger/fabric v1.4.0-rc1.0.20230405174026-695dd57e01c2
--- token/services/network/fabric/finality/delivery.go
+++ token/services/network/fabric/finality/delivery.go
@@ -10,12 +10,24 @@ import (
 	"context"
 
 	"github.com/hyperledger-labs/fabric-smart-client/platform/fabric"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/logging"
 )
 
 type Delivery struct {
 	*fabric.Delivery
+	*fabric.Ledger
+	Logger logging.Logger
 }
 
 func (d *Delivery) ScanBlock(background context.Context, callback fabric.BlockCallback) error {
-	return d.Delivery.ScanBlock(background, callback)
+	startingBlock := uint64(0)
+	if d.Ledger != nil {
+		info, err := d.Ledger.GetLedgerInfo()
+		if err == nil {
+			startingBlock = info.Height
+		} else {
+			d.Logger.Errorf("failed to get ledger info: %s", err)
+		}
+	}
+	return d.Delivery.ScanBlockFrom(background, startingBlock, callback)
 }
