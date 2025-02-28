8253d88357d044491de45e2cc32419be9088feaa
Angelo De Caro <adc@zurich.ibm.com>
2025-02-28 13:36:39 +0100

    removed fabric msp
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/identity/msp/x509/msp/ecdsa.go
+++ token/services/identity/msp/x509/msp/ecdsa.go
@@ -7,13 +7,17 @@ SPDX-License-Identifier: Apache-2.0
 package msp
 
 import (
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/sha256"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/asn1"
 	"encoding/pem"
+	"fmt"
 	"math/big"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -31,7 +35,14 @@ var (
 	}
 )
 
-type ecdsaSignature struct {
+func isECDSASignedCert(cert *x509.Certificate) bool {
+	return cert.SignatureAlgorithm == x509.ECDSAWithSHA1 ||
+		cert.SignatureAlgorithm == x509.ECDSAWithSHA256 ||
+		cert.SignatureAlgorithm == x509.ECDSAWithSHA384 ||
+		cert.SignatureAlgorithm == x509.ECDSAWithSHA512
+}
+
+type ECDSASignature struct {
 	R, S *big.Int
 }
 
@@ -44,7 +55,7 @@ func NewECDSAVerifier(pk *ecdsa.PublicKey) *ecdsaVerifier {
 }
 
 func (d *ecdsaVerifier) Verify(message, sigma []byte) error {
-	signature := &ecdsaSignature{}
+	signature := &ECDSASignature{}
 	_, err := asn1.Unmarshal(sigma, signature)
 	if err != nil {
 		return err
@@ -118,4 +129,164 @@ func PemDecodeKey(keyBytes []byte) (interface{}, error) {
 	}
 
 	return key, nil
+}
+
+func SignatureToLowS(k *ecdsa.PublicKey, signature []byte) ([]byte, error) {
+	r, s, err := UnmarshalECDSASignature(signature)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err = ToLowS(k, s)
+	if err != nil {
+		return nil, err
+	}
+
+	return MarshalECDSASignature(r, s)
+}
+func ToLowS(k *ecdsa.PublicKey, s *big.Int) (*big.Int, error) {
+	lowS, err := IsLowS(k, s)
+	if err != nil {
+		return nil, err
+	}
+
+	if !lowS {
+		// Set s to N - s that will be then in the lower part of signature space
+		// less or equal to half order
+		s.Sub(k.Params().N, s)
+
+		return s, nil
+	}
+
+	return s, nil
+}
+
+func MarshalECDSASignature(r, s *big.Int) ([]byte, error) {
+	return asn1.Marshal(ECDSASignature{r, s})
+}
+
+func UnmarshalECDSASignature(raw []byte) (*big.Int, *big.Int, error) {
+	// Unmarshal
+	sig := new(ECDSASignature)
+	_, err := asn1.Unmarshal(raw, sig)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed unmashalling signature [%s]", err)
+	}
+
+	// Validate sig
+	if sig.R == nil {
+		return nil, nil, errors.New("invalid signature, R must be different from nil")
+	}
+	if sig.S == nil {
+		return nil, nil, errors.New("invalid signature, S must be different from nil")
+	}
+
+	if sig.R.Sign() != 1 {
+		return nil, nil, errors.New("invalid signature, R must be larger than zero")
+	}
+	if sig.S.Sign() != 1 {
+		return nil, nil, errors.New("invalid signature, S must be larger than zero")
+	}
+
+	return sig.R, sig.S, nil
+}
+
+// sanitizeECDSASignedCert checks that the signatures signing a cert
+// is in low-S. This is checked against the public key of parentCert.
+// If the signature is not in low-S, then a new certificate is generated
+// that is equals to cert but the signature that is in low-S.
+func sanitizeECDSASignedCert(cert *x509.Certificate, parentCert *x509.Certificate) (*x509.Certificate, error) {
+	if cert == nil {
+		return nil, errors.New("certificate must be different from nil")
+	}
+	if parentCert == nil {
+		return nil, errors.New("parent certificate must be different from nil")
+	}
+
+	expectedSig, err := SignatureToLowS(parentCert.PublicKey.(*ecdsa.PublicKey), cert.Signature)
+	if err != nil {
+		return nil, err
+	}
+
+	// if sig == cert.Signature, nothing needs to be done
+	if bytes.Equal(cert.Signature, expectedSig) {
+		return cert, nil
+	}
+	// otherwise create a new certificate with the new signature
+
+	// 1. Unmarshal cert.Raw to get an instance of certificate,
+	//    the lower level interface that represent an x509 certificate
+	//    encoding
+	var newCert certificate
+	newCert, err = certFromX509Cert(cert)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. Change the signature
+	newCert.SignatureValue = asn1.BitString{Bytes: expectedSig, BitLength: len(expectedSig) * 8}
+	newCert.Raw = nil
+
+	// 3. marshal again newCert. Raw must be nil
+	newRaw, err := asn1.Marshal(newCert)
+	if err != nil {
+		return nil, errors.Wrap(err, "marshalling of the certificate failed")
+	}
+
+	// 4. parse newRaw to get an x509 certificate
+	return x509.ParseCertificate(newRaw)
+}
+
+type validity struct {
+	NotBefore, NotAfter time.Time
+}
+
+type publicKeyInfo struct {
+	Raw       asn1.RawContent
+	Algorithm pkix.AlgorithmIdentifier
+	PublicKey asn1.BitString
+}
+
+type certificate struct {
+	Raw                asn1.RawContent
+	TBSCertificate     tbsCertificate
+	SignatureAlgorithm pkix.AlgorithmIdentifier
+	SignatureValue     asn1.BitString
+}
+
+type tbsCertificate struct {
+	Raw                asn1.RawContent
+	Version            int `asn1:"optional,explicit,default:0,tag:0"`
+	SerialNumber       *big.Int
+	SignatureAlgorithm pkix.AlgorithmIdentifier
+	Issuer             asn1.RawValue
+	Validity           validity
+	Subject            asn1.RawValue
+	PublicKey          publicKeyInfo
+	UniqueId           asn1.BitString   `asn1:"optional,tag:1"`
+	SubjectUniqueId    asn1.BitString   `asn1:"optional,tag:2"`
+	Extensions         []pkix.Extension `asn1:"optional,explicit,tag:3"`
+}
+
+func certFromX509Cert(cert *x509.Certificate) (certificate, error) {
+	var newCert certificate
+	_, err := asn1.Unmarshal(cert.Raw, &newCert)
+	if err != nil {
+		return certificate{}, errors.Wrap(err, "unmarshalling of the certificate failed")
+	}
+	return newCert, nil
+}
+
+// String returns a PEM representation of a certificate
+func (c certificate) String() string {
+	b, err := asn1.Marshal(c)
+	if err != nil {
+		return fmt.Sprintf("Failed marshaling cert: %v", err)
+	}
+	block := &pem.Block{
+		Bytes: b,
+		Type:  "CERTIFICATE",
+	}
+	b = pem.EncodeToMemory(block)
+	return string(b)
 }
--- token/services/identity/msp/x509/msp/setup.go
+++ token/services/identity/msp/x509/msp/setup.go
@@ -7,123 +7,58 @@ SPDX-License-Identifier: Apache-2.0
 package msp
 
 import (
-	"encoding/pem"
-
 	"github.com/hyperledger-labs/fabric-smart-client/pkg/utils/proto"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/driver"
 	msp2 "github.com/hyperledger/fabric-protos-go/msp"
 	"github.com/hyperledger/fabric/bccsp"
-	"github.com/hyperledger/fabric/msp"
 	"github.com/pkg/errors"
 )
 
 func SerializeFromMSP(conf *msp2.MSPConfig, mspID string) ([]byte, error) {
-	msp, err := loadVerifyingMSPAt(conf)
+	factory, err := getIdentityFactory(nil, nil)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to load msp at [%s]", mspID)
+		return nil, errors.WithMessage(err, "failed to get identity factory")
 	}
-	certRaw, err := loadLocalMSPSignerCert(conf)
+	identityInfo, err := getSigningIdentityInfo(conf)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to load certificate at [%s]", mspID)
+		return nil, errors.WithMessage(err, "failed to get signing identity info")
 	}
-	serRaw, err := serializeRaw(mspID, certRaw)
+	signingIdentity, err := factory.GetIdentity(identityInfo)
 	if err != nil {
-		return nil, errors.WithMessagef(err, "failed to generate msp serailization at [%s]", mspID)
+		return nil, errors.WithMessage(err, "failed to get signing identity")
 	}
-	id, err := msp.DeserializeIdentity(serRaw)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to deserialize certificate at [%s]", mspID)
-	}
-	return id.Serialize()
+	return signingIdentity.Serialize()
 }
 
 // GetSigningIdentity retrieves a signing identity from the passed arguments.
 // If keyStorePath is empty, then it is assumed that the key is at mspConfigPath/keystore
 func GetSigningIdentity(conf *msp2.MSPConfig, bccspConfig *BCCSP, keyStore bccsp.KeyStore) (driver.FullIdentity, error) {
-	mspInstance, err := loadLocalMSPAt(conf, bccspConfig, keyStore)
+	factory, err := getIdentityFactory(bccspConfig, keyStore)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithMessage(err, "failed to get identity factory")
 	}
-
-	signingIdentity, err := mspInstance.GetDefaultSigningIdentity()
+	identityInfo, err := getSigningIdentityInfo(conf)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithMessage(err, "failed to get signing identity info")
 	}
-
+	signingIdentity, err := factory.GetFullIdentity(identityInfo)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to get signing identity")
+	}
 	return signingIdentity, nil
 }
 
-// loadVerifyingMSPAt loads a verifying MSP whose configuration is stored at 'dir', and whose
+// getIdentityFactory loads an MSP whose configuration is stored at 'dir', and whose
 // id and type are the passed as arguments.
-func loadVerifyingMSPAt(conf *msp2.MSPConfig) (msp.MSP, error) {
-	cp, err := GetBCCSPFromConf(nil, nil)
-	if err != nil {
-		return nil, errors.WithMessagef(err, "failed to get bccsp")
-	}
-
-	mspOpts := &msp.BCCSPNewOpts{
-		NewBaseOpts: msp.NewBaseOpts{
-			Version: msp.MSPv1_0,
-		},
-	}
-	thisMSP, err := msp.New(mspOpts, cp)
-	if err != nil {
-		return nil, errors.WithMessagef(err, "failed to create new BCCSPMSP instance")
-	}
-	err = thisMSP.Setup(conf)
-	if err != nil {
-		return nil, errors.WithMessagef(err, "failed to setup the new BCCSPMSP instance")
-	}
-	return thisMSP, nil
-}
-
-func serializeRaw(mspID string, raw []byte) ([]byte, error) {
-	cert, err := getCertFromPem(raw)
-	if err != nil {
-		return nil, err
-	}
-
-	pb := &pem.Block{Bytes: cert.Raw, Type: "CERTIFICATE"}
-	pemBytes := pem.EncodeToMemory(pb)
-	if pemBytes == nil {
-		return nil, errors.New("encoding of identity failed")
-	}
-
-	// We serialize identities by prepending the MSPID and appending the ASN.1 DER content of the cert
-	sID := &msp2.SerializedIdentity{Mspid: mspID, IdBytes: pemBytes}
-	idBytes, err := proto.Marshal(sID)
-	if err != nil {
-		return nil, errors.Wrapf(err, "could not marshal a SerializedIdentity structure for identity %s", mspID)
-	}
-
-	return idBytes, nil
-}
-
-// loadLocalMSPAt loads an MSP whose configuration is stored at 'dir', and whose
-// id and type are the passed as arguments.
-func loadLocalMSPAt(conf *msp2.MSPConfig, bccspConfig *BCCSP, keyStore bccsp.KeyStore) (msp.MSP, error) {
-	cp, err := GetBCCSPFromConf(bccspConfig, keyStore)
+func getIdentityFactory(bccspConfig *BCCSP, keyStore bccsp.KeyStore) (*IdentityFactory, error) {
+	csp, err := GetBCCSPFromConf(bccspConfig, keyStore)
 	if err != nil {
 		return nil, errors.WithMessagef(err, "failed to get bccsp from config [%v]", bccspConfig)
 	}
-
-	mspOpts := &msp.BCCSPNewOpts{
-		NewBaseOpts: msp.NewBaseOpts{
-			Version: msp.MSPv1_0,
-		},
-	}
-	thisMSP, err := msp.New(mspOpts, cp)
-	if err != nil {
-		return nil, errors.WithMessagef(err, "failed to create new BCCSPMSP instance")
-	}
-	err = thisMSP.Setup(conf)
-	if err != nil {
-		return nil, errors.WithMessagef(err, "failed to setup the new BCCSPMSP instance")
-	}
-	return thisMSP, nil
+	return NewIdentityFactory(csp, bccsp.SHA2), nil
 }
 
-func loadLocalMSPSignerCert(conf *msp2.MSPConfig) ([]byte, error) {
+func getSigningIdentityInfo(conf *msp2.MSPConfig) (*msp2.SigningIdentityInfo, error) {
 	c := &msp2.FabricMSPConfig{}
 	if err := proto.Unmarshal(conf.Config, c); err != nil {
 		return nil, errors.WithMessagef(err, "failed to load provider config [%v]", conf.Config)
@@ -131,5 +66,5 @@ func loadLocalMSPSignerCert(conf *msp2.MSPConfig) ([]byte, error) {
 	if c.SigningIdentity == nil {
 		return nil, errors.Errorf("signing identity is missing")
 	}
-	return c.SigningIdentity.PublicSigner, nil
+	return c.SigningIdentity, nil
 }
