00ae179aabb31ffb9132082ee55a9f618b92046d
Angelo De Caro <adc@zurich.ibm.com>
2025-02-28 13:36:39 +0100

    removed fabric msp
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ token/services/identity/msp/x509/msp/msp.go
@@ -0,0 +1,304 @@
+package msp
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+
+	"github.com/hyperledger-labs/fabric-smart-client/pkg/utils/proto"
+	m "github.com/hyperledger/fabric-protos-go/msp"
+	"github.com/hyperledger/fabric/bccsp"
+	"github.com/hyperledger/fabric/bccsp/signer"
+	"github.com/pkg/errors"
+)
+
+type verifyingIdentity struct {
+	bccsp               bccsp.BCCSP
+	SignatureHashFamily string
+	cert                *x509.Certificate
+	Mspid               string
+	pk                  bccsp.Key
+}
+
+func (f *verifyingIdentity) Serialize() ([]byte, error) {
+	pb := &pem.Block{Bytes: f.cert.Raw, Type: "CERTIFICATE"}
+	pemBytes := pem.EncodeToMemory(pb)
+	if pemBytes == nil {
+		return nil, errors.New("encoding of identity failed")
+	}
+
+	// We serialize identities by prepending the MSPID and appending the ASN.1 DER content of the cert
+	sId := &m.SerializedIdentity{Mspid: f.Mspid, IdBytes: pemBytes}
+	idBytes, err := proto.Marshal(sId)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed serializing identity")
+	}
+
+	return idBytes, nil
+}
+
+func (f *verifyingIdentity) Verify(message, sigma []byte) error {
+	hashOpt, err := getHashOpt(f.SignatureHashFamily)
+	if err != nil {
+		return errors.WithMessage(err, "failed getting hash function options")
+	}
+
+	digest, err := f.bccsp.Hash(message, hashOpt)
+	if err != nil {
+		return errors.WithMessage(err, "failed computing digest")
+	}
+
+	valid, err := f.bccsp.Verify(f.pk, sigma, digest, nil)
+	if err != nil {
+		return errors.WithMessage(err, "could not determine the validity of the signature")
+	} else if !valid {
+		return errors.New("signature is invalid")
+	}
+
+	return nil
+}
+
+type fullIdentity struct {
+	*verifyingIdentity
+	signer crypto.Signer
+}
+
+func (f *fullIdentity) Sign(msg []byte) ([]byte, error) {
+	// Compute Hash
+	hashOpt, err := getHashOpt(f.SignatureHashFamily)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed getting hash function options")
+	}
+
+	digest, err := f.bccsp.Hash(msg, hashOpt)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed computing digest")
+	}
+
+	// Sign
+	return f.signer.Sign(rand.Reader, digest, nil)
+}
+
+func (f *fullIdentity) Serialize() ([]byte, error) {
+	pb := &pem.Block{Bytes: f.cert.Raw, Type: "CERTIFICATE"}
+	pemBytes := pem.EncodeToMemory(pb)
+	if pemBytes == nil {
+		return nil, errors.New("encoding of identity failed")
+	}
+
+	// We serialize identities by prepending the MSPID and appending the ASN.1 DER content of the cert
+	sId := &m.SerializedIdentity{Mspid: f.Mspid, IdBytes: pemBytes}
+	idBytes, err := proto.Marshal(sId)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed serializing identity")
+	}
+
+	return idBytes, nil
+}
+
+func (f *fullIdentity) Verify(message, sigma []byte) error {
+	hashOpt, err := getHashOpt(f.SignatureHashFamily)
+	if err != nil {
+		return errors.WithMessage(err, "failed getting hash function options")
+	}
+
+	digest, err := f.bccsp.Hash(message, hashOpt)
+	if err != nil {
+		return errors.WithMessage(err, "failed computing digest")
+	}
+
+	valid, err := f.bccsp.Verify(f.pk, sigma, digest, nil)
+	if err != nil {
+		return errors.WithMessage(err, "could not determine the validity of the signature")
+	} else if !valid {
+		return errors.New("signature is invalid")
+	}
+
+	return nil
+}
+
+type IdentityFactory struct {
+	bccsp               bccsp.BCCSP
+	SignatureHashFamily string
+}
+
+func NewIdentityFactory(bccsp bccsp.BCCSP, signatureHashFamily string) *IdentityFactory {
+	return &IdentityFactory{bccsp: bccsp, SignatureHashFamily: signatureHashFamily}
+}
+
+func (f *IdentityFactory) GetFullIdentity(sidInfo *m.SigningIdentityInfo) (*fullIdentity, error) {
+	if sidInfo == nil {
+		return nil, errors.New("getIdentityFromBytes error: nil sidInfo")
+	}
+
+	// Extract the public part of the identity
+	idPub, pubKey, err := f.getIdentityFromConf(sidInfo.PublicSigner)
+	if err != nil {
+		return nil, err
+	}
+
+	// Find the matching private key in the BCCSP keystore
+	privKey, err := f.bccsp.GetKey(pubKey.SKI())
+	// Less Secure: Attempt to import Private Key from KeyInfo, if BCCSP was not able to find the key
+	if err != nil {
+		if sidInfo.PrivateSigner == nil || sidInfo.PrivateSigner.KeyMaterial == nil {
+			return nil, errors.New("KeyMaterial not found in SigningIdentityInfo")
+		}
+		pemKey, _ := pem.Decode(sidInfo.PrivateSigner.KeyMaterial)
+		if pemKey == nil {
+			return nil, errors.Errorf("%s: wrong PEM encoding", sidInfo.PrivateSigner.KeyIdentifier)
+		}
+		privKey, err = f.bccsp.KeyImport(pemKey.Bytes, &bccsp.ECDSAPrivateKeyImportOpts{Temporary: true})
+		if err != nil {
+			return nil, errors.WithMessage(err, "getIdentityFromBytes error: Failed to import EC private key")
+		}
+	}
+
+	// get the peer signer
+	identitySigner, err := signer.New(f.bccsp, privKey)
+	if err != nil {
+		return nil, errors.WithMessage(err, "getIdentityFromBytes error: Failed initializing bccspCryptoSigner")
+	}
+
+	return &fullIdentity{
+		verifyingIdentity: &verifyingIdentity{
+			bccsp:               f.bccsp,
+			SignatureHashFamily: f.SignatureHashFamily,
+			Mspid:               "",
+			cert:                idPub,
+			pk:                  pubKey,
+		},
+		signer: identitySigner,
+	}, nil
+}
+
+func (f *IdentityFactory) GetIdentity(sidInfo *m.SigningIdentityInfo) (*verifyingIdentity, error) {
+	if sidInfo == nil {
+		return nil, errors.New("getIdentityFromBytes error: nil sidInfo")
+	}
+
+	// Extract the public part of the identity
+	idPub, pubKey, err := f.getIdentityFromConf(sidInfo.PublicSigner)
+	if err != nil {
+		return nil, err
+	}
+
+	return &verifyingIdentity{
+		bccsp:               f.bccsp,
+		SignatureHashFamily: f.SignatureHashFamily,
+		Mspid:               "",
+		cert:                idPub,
+		pk:                  pubKey,
+	}, nil
+}
+
+func (f *IdentityFactory) getIdentityFromConf(idBytes []byte) (*x509.Certificate, bccsp.Key, error) {
+	// get a cert
+	cert, err := f.getCertFromPem(idBytes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// get the public key in the right format
+	certPubK, err := f.bccsp.KeyImport(cert, &bccsp.X509PublicKeyImportOpts{Temporary: true})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return cert, certPubK, nil
+}
+
+func (f *IdentityFactory) getCertFromPem(idBytes []byte) (*x509.Certificate, error) {
+	if idBytes == nil {
+		return nil, errors.New("getCertFromPem error: nil idBytes")
+	}
+
+	// Decode the pem bytes
+	pemCert, _ := pem.Decode(idBytes)
+	if pemCert == nil {
+		return nil, errors.Errorf("getCertFromPem error: could not decode pem bytes [%v]", idBytes)
+	}
+
+	// get a cert
+	var cert *x509.Certificate
+	cert, err := x509.ParseCertificate(pemCert.Bytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "getCertFromPem error: failed to parse x509 cert")
+	}
+
+	return cert, nil
+}
+
+func (f *IdentityFactory) sanitizeCert(cert *x509.Certificate) (*x509.Certificate, error) {
+	return cert, nil
+	// var err error
+	// if isECDSASignedCert(cert) {
+	// 	isRootCACert := false
+	// 	validityOpts := f.getValidityOptsForCert(cert)
+	// 	if cert.IsCA && cert.CheckSignatureFrom(cert) == nil {
+	// 		// this is a root CA we can already sanitize it
+	// 		cert, err = sanitizeECDSASignedCert(cert, cert)
+	// 		if err != nil {
+	// 			return nil, err
+	// 		}
+	// 		isRootCACert = true
+	// 		validityOpts.Roots = x509.NewCertPool()
+	// 		validityOpts.Roots.AddCert(cert)
+	// 	}
+	// 	// Lookup for a parent certificate to perform the sanitization
+	// 	// run cert validation at any rate, if this is a root CA
+	// 	// we will validate already sanitized cert
+	// 	chain, err := f.getUniqueValidationChain(cert, validityOpts)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	//
+	// 	// once we finish validation and this is already
+	// 	// sanitized certificate, there is no need to
+	// 	// sanitize it once again hence we can just return it
+	// 	if isRootCACert {
+	// 		return cert, nil
+	// 	}
+	//
+	// 	// ok, this is no a root CA cert, and now we
+	// 	// have chain of certs and can extract parent
+	// 	// to sanitize the cert whenever it's intermediate or leaf certificate
+	// 	var parentCert *x509.Certificate
+	// 	if len(chain) <= 1 {
+	// 		return nil, fmt.Errorf("failed to traverse certificate verification chain"+
+	// 			" for leaf or intermediate certificate, with subject %s", cert.Subject)
+	// 	}
+	// 	parentCert = chain[1]
+	//
+	// 	// Sanitize
+	// 	return sanitizeECDSASignedCert(cert, parentCert)
+	// }
+	// return cert, nil
+}
+
+func (f *IdentityFactory) getValidityOptsForCert(cert *x509.Certificate) x509.VerifyOptions {
+	// First copy the opts to override the CurrentTime field
+	// in order to make the certificate passing the expiration test
+	// independently from the real local current time.
+	// This is a temporary workaround for FAB-3678
+
+	var tempOpts x509.VerifyOptions
+	// tempOpts.Roots = f.opts.Roots
+	// tempOpts.DNSName = f.opts.DNSName
+	// tempOpts.Intermediates = f.opts.Intermediates
+	// tempOpts.KeyUsages = f.opts.KeyUsages
+	// tempOpts.CurrentTime = cert.NotBefore.Add(time.Second)
+
+	return tempOpts
+}
+
+func getHashOpt(hashFamily string) (bccsp.HashOpts, error) {
+	switch hashFamily {
+	case bccsp.SHA2:
+		return bccsp.GetHashOpt(bccsp.SHA256)
+	case bccsp.SHA3:
+		return bccsp.GetHashOpt(bccsp.SHA3_256)
+	}
+	return nil, errors.Errorf("hash family not recognized [%s]", hashFamily)
+}
