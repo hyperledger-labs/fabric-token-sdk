b53ae1d134c7823b75f146a98f633082a1de8cd0
akram <akram@il.ibm.com>
2025-03-10 01:45:27 +0200

    Rollback the method to pull and run  Hashicorp Vault
    Signed-off-by: akram <akram@il.ibm.com>

--- token/services/db/sql/common/test_utils.go
+++ token/services/db/sql/common/test_utils.go
@@ -8,17 +8,11 @@ package common
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"os"
 	"runtime/debug"
 	"testing"
 	"time"
 
-	"github.com/docker/docker/api/types/container"
-	containertypes "github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/image"
-	"github.com/docker/docker/client"
 	postgres2 "github.com/hyperledger-labs/fabric-smart-client/platform/view/services/db/driver/sql/postgres"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -64,43 +58,4 @@ func StartPostgresContainer(t *testing.T) (func(), string) {
 			logger.Errorf("failed to terminate [%s][%s]", err, debug.Stack())
 		}
 	}, pgConnStr
-}
-
-func StartHashicorpVaultContainer(t *testing.T) func() {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer cli.Close()
-
-	imageName := "hashicorp/vault"
-
-	out, err := cli.ImagePull(ctx, imageName, image.PullOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer out.Close()
-	io.Copy(os.Stdout, out)
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: imageName,
-	}, nil, nil, nil, "")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
-		t.Fatal(err)
-	}
-
-	fmt.Println(resp.ID)
-
-	return func() {
-		noWaitTimeout := 0 // to not wait for the container to exit gracefully
-		if err := cli.ContainerStop(ctx, resp.ID, containertypes.StopOptions{Timeout: &noWaitTimeout}); err != nil {
-			logger.Errorf("failed to terminate hashicorp/vault [%s][%s]", err, debug.Stack())
-		}
-		fmt.Println("Success")
-	}
 }
