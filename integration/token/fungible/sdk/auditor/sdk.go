0f4fc9c0bc04bab8cd9adce8f14d5e6b7d874f27
Angelo De Caro <adc@zurich.ibm.com>
2025-03-12 07:15:17 +0100

    simplify the business sdks thanks to the features added to FSC
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- integration/token/fungible/sdk/auditor/sdk.go
+++ integration/token/fungible/sdk/auditor/sdk.go
@@ -7,11 +7,8 @@ SPDX-License-Identifier: Apache-2.0
 package auditor
 
 import (
-	"context"
 	errors2 "errors"
-	"reflect"
 
-	"github.com/hyperledger-labs/fabric-smart-client/pkg/node"
 	dig2 "github.com/hyperledger-labs/fabric-smart-client/platform/common/sdk/dig"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/driver"
 	views1 "github.com/hyperledger-labs/fabric-token-sdk/integration/token/common/views"
@@ -22,11 +19,6 @@ import (
 
 type SDK struct {
 	dig2.SDK
-	registry node.Registry
-}
-
-func NewSDK(registry node.Registry) *SDK {
-	return &SDK{registry: registry}
 }
 
 func NewFrom(sdk dig2.SDK) *SDK {
@@ -34,30 +26,10 @@ func NewFrom(sdk dig2.SDK) *SDK {
 }
 
 func (p *SDK) Install() error {
-	// get dig from registry, this was installed by the FTS's sdk
-	var digContainer *dig.Container
-	if p.SDK != nil {
-		if err := p.SDK.Install(); err != nil {
-			return err
-		}
-		digContainer = p.SDK.Container()
-	} else {
-		// get it from the registry
-		if p.registry == nil {
-			return errors.New("registry not set")
-		}
-		wrapper, err := p.registry.GetService(reflect.TypeOf((*dig.Container)(nil)))
-		if err != nil {
-			return errors.WithMessage(err, "failed getting dig container service")
-		}
-		container, ok := wrapper.(*dig.Container)
-		if !ok {
-			return errors.New("failed getting dig container service, not an instance of *dig.Container")
-		}
-		digContainer = container
+	if err := p.SDK.Install(); err != nil {
+		return err
 	}
-
-	if err := digContainer.Invoke(func(in struct {
+	if err := p.SDK.Container().Invoke(func(in struct {
 		dig.In
 		Registry driver.Registry // replace this with an external interface
 	}) error {
@@ -81,25 +53,6 @@ func (p *SDK) Install() error {
 		)
 	}); err != nil {
 		return errors.WithMessage(err, "failed to install auditor's views")
-	}
-	return nil
-}
-
-func (p *SDK) Start(ctx context.Context) error {
-	if p.SDK != nil {
-		if err := p.SDK.Start(ctx); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (p *SDK) PostStart(ctx context.Context) error {
-	if p.SDK != nil {
-		ps, ok := p.SDK.(node.PostStart)
-		if ok {
-			return ps.PostStart(ctx)
-		}
 	}
 	return nil
 }
