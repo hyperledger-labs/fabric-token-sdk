a84997b03e97835b74aaba64b6ba0bf3ac74f27e
Angelo De Caro <adc@zurich.ibm.com>
2025-03-12 14:31:27 +0100

    pp marshalling with testing
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/common/encoding/pp/pp.go
+++ token/core/common/encoding/pp/pp.go
@@ -1,15 +1,26 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package pp
 
 import (
+	"github.com/hyperledger-labs/fabric-smart-client/pkg/utils/errors"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/core/common/encoding/json"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/driver/protos-go/pp"
 )
 
 // Marshal marshals the passed public parameters
 func Marshal(pp *pp.PublicParameters) ([]byte, error) {
+	if pp == nil {
+		return nil, errors.New("nil public parameters")
+	}
 	return json.Marshal(pp)
 }
 
+// Unmarshal unmarshals the passed slice into an instance of pp.PublicParameters
 func Unmarshal(raw []byte) (*pp.PublicParameters, error) {
 	pp := &pp.PublicParameters{}
 	if err := json.Unmarshal(raw, pp); err != nil {
