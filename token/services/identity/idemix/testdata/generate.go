3812ddfc5d56191c02254028a6052b4a967fc3af
Angelo De Caro <adc@zurich.ibm.com>
2025-03-13 08:57:44 +0100

    lint fix
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    Signed-off-by: akram <akram@il.ibm.com>

--- token/services/identity/idemix/testdata/generate.go
+++ token/services/identity/idemix/testdata/generate.go
@@ -1,3 +1,9 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package testdata
 
 //go:generate idemixgen ca-keygen --output ./bls12_381_bbs/ca --curve BLS12_381_BBS --aries
