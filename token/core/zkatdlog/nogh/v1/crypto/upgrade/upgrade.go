22efc72bd66ff55b04492c6d8338d7728c8ab739
Angelo De Caro <adc@zurich.ibm.com>
2025-03-17 09:48:51 +0100

    sketch
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/zkatdlog/nogh/v1/crypto/upgrade/upgrade.go
+++ token/core/zkatdlog/nogh/v1/crypto/upgrade/upgrade.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package upgrade
 
 import (
+	"bytes"
 	"crypto/rand"
 	errors2 "errors"
 
@@ -59,6 +60,10 @@ func (p *Proof) SHA256Digest() ([]byte, error) {
 	return h.Digest(), nil
 }
 
+func (p *Proof) AddSignature(sigma Signature) {
+	p.Signatures = append(p.Signatures, sigma)
+}
+
 type Service struct{}
 
 func NewService() *Service {
@@ -82,11 +87,76 @@ func (s *Service) NewUpgradeChallenge() (driver.TokensUpgradeChallenge, error) {
 // GenUpgradeProof does the following: For each token in input, it signs the concatenation of the challenge and the tokens to be upgraded.
 // These signatures are then added to the proof
 func (s *Service) GenUpgradeProof(ch driver.TokensUpgradeChallenge, tokens []token.LedgerToken, witness driver.TokensUpgradeWitness) (driver.TokensUpgradeProof, error) {
-	// TODO: implement
-	return nil, nil
+	if len(ch) != ChallengeSize {
+		return nil, errors.Errorf("invalid challenge size, got [%d], expected [%d]", len(ch), ChallengeSize)
+	}
+	if len(tokens) == 0 {
+		return nil, errors.Errorf("no tokens provided")
+	}
+
+	proof := &Proof{
+		Challenge:  ch,
+		Tokens:     tokens,
+		Signatures: make([]Signature, 0, len(tokens)),
+	}
+	digest, err := proof.SHA256Digest()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get sha256 digest")
+	}
+
+	for _, token := range tokens {
+		// get a signer for each token
+
+		// sign the digest
+		var sigma Signature
+
+		// add the signature to the proof
+		proof.AddSignature(sigma)
+	}
+
+	// marshal proof
+	raw, err := proof.Serialize()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to serialize proof")
+	}
+	return raw, nil
 }
 
-func (s *Service) CheckUpgradeProof(ch driver.TokensUpgradeChallenge, proof driver.TokensUpgradeProof, tokens []token.LedgerToken) (bool, error) {
-	// TODO: implement
+func (s *Service) CheckUpgradeProof(ch driver.TokensUpgradeChallenge, proofRaw driver.TokensUpgradeProof, tokens []token.LedgerToken) (bool, error) {
+	if len(ch) != ChallengeSize {
+		return false, errors.Errorf("invalid challenge size, got [%d], expected [%d]", len(ch), ChallengeSize)
+	}
+	if len(tokens) == 0 {
+		return false, errors.Errorf("no tokens provided")
+	}
+	if len(proofRaw) == 0 {
+		return false, errors.Errorf("no proof provided")
+	}
+
+	// unmarshal proof
+	proof := &Proof{}
+	if err := proof.Deserialize(proofRaw); err != nil {
+		return false, errors.Wrapf(err, "failed to deserialize proof")
+	}
+	if len(proof.Tokens) != len(tokens) {
+		return false, errors.Errorf("invalid token count")
+	}
+	if !bytes.Equal(proof.Challenge, ch) {
+		return false, errors.Errorf("invalid challenge")
+	}
+	proof.Challenge = ch
+	proof.Tokens = tokens
+
+	digest, err := proof.SHA256Digest()
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to get sha256 digest")
+	}
+
+	// verify signatures
+	for i, token := range proof.Tokens {
+
+	}
+
+	// all good
 	return true, nil
 }
