8cbe401c8535f75cb3dfcc92d14324d2addeb843
Angelo De Caro <angelo.decaro@gmail.com>
2025-03-25 07:59:06 +0100

    cleanup
    Signed-off-by: Angelo De Caro <angelo.decaro@gmail.com>

--- token/core/fabtoken/v1/core/setup.go
+++ token/core/fabtoken/v1/core/setup.go
@@ -66,117 +66,127 @@ func NewPublicParamsFromBytes(raw []byte, label string) (*PublicParams, error) {
 
 // Identifier returns the label associated with the PublicParams
 // todo shall we used Identifier instead of Label?
-func (pp *PublicParams) Identifier() string {
-	return pp.Label
+func (p *PublicParams) Identifier() string {
+	return p.Label
 }
 
-func (pp *PublicParams) Version() string {
-	return pp.Ver
+func (p *PublicParams) Version() string {
+	return p.Ver
 }
 
 // TokenDataHiding indicates if the PublicParams corresponds to a driver that hides token data
 // fabtoken does not hide token data, hence, TokenDataHiding returns false
-func (pp *PublicParams) TokenDataHiding() bool {
+func (p *PublicParams) TokenDataHiding() bool {
 	return false
 }
 
 // CertificationDriver returns the label of the PublicParams
 // From the label, one can deduce what certification process will be used if any.
-func (pp *PublicParams) CertificationDriver() string {
-	return pp.Label
+func (p *PublicParams) CertificationDriver() string {
+	return p.Label
 }
 
 // GraphHiding indicates if the PublicParams corresponds to a driver that hides the transaction graph
 // fabtoken does not hide the graph, hence, GraphHiding returns false
-func (pp *PublicParams) GraphHiding() bool {
+func (p *PublicParams) GraphHiding() bool {
 	return false
 }
 
 // MaxTokenValue returns the maximum value that a token can hold according to PublicParams
-func (pp *PublicParams) MaxTokenValue() uint64 {
-	return pp.MaxToken
+func (p *PublicParams) MaxTokenValue() uint64 {
+	return p.MaxToken
 }
 
 // Bytes marshals PublicParams
-func (pp *PublicParams) Bytes() ([]byte, error) {
-	return json.Marshal(pp)
+func (p *PublicParams) Bytes() ([]byte, error) {
+	return json.Marshal(p)
 }
 
 // Serialize marshals a wrapper around PublicParams (SerializedPublicParams)
-func (pp *PublicParams) Serialize() ([]byte, error) {
-	raw, err := json.Marshal(pp)
+func (p *PublicParams) Serialize() ([]byte, error) {
+	raw, err := json.Marshal(p)
 	if err != nil {
 		return nil, err
 	}
 	return json.Marshal(&pp2.PublicParameters{
-		Identifier: pp.Label,
+		Identifier: p.Label,
 		Raw:        raw,
 	})
 }
 
 // Deserialize un-marshals the passed bytes into PublicParams
-func (pp *PublicParams) Deserialize(raw []byte) error {
+func (p *PublicParams) Deserialize(raw []byte) error {
 	publicParams := &pp2.PublicParameters{}
 	if err := json.Unmarshal(raw, publicParams); err != nil {
 		return err
 	}
-	if publicParams.Identifier != pp.Label {
+	if publicParams.Identifier != p.Label {
 		return errors.Errorf("invalid identifier, expecting 'fabtoken', got [%s]", publicParams.Raw)
 	}
-	return json.Unmarshal(publicParams.Raw, pp)
+	return json.Unmarshal(publicParams.Raw, p)
 }
 
 // AuditorIdentity returns the auditor identity encoded in PublicParams
-func (pp *PublicParams) AuditorIdentity() driver.Identity {
-	return pp.Auditor
+func (p *PublicParams) AuditorIdentity() driver.Identity {
+	return p.Auditor
 }
 
 // AddAuditor sets the Auditor field in PublicParams to the passed identity
-func (pp *PublicParams) AddAuditor(auditor driver.Identity) {
-	pp.Auditor = auditor
+func (p *PublicParams) AddAuditor(auditor driver.Identity) {
+	p.Auditor = auditor
 }
 
 // AddIssuer adds the passed issuer to the array of Issuers in PublicParams
-func (pp *PublicParams) AddIssuer(issuer driver.Identity) {
-	pp.IssuerIDs = append(pp.IssuerIDs, issuer)
+func (p *PublicParams) AddIssuer(issuer driver.Identity) {
+	p.IssuerIDs = append(p.IssuerIDs, issuer)
+}
+
+// SetIssuers sets the issuers to the passed identities
+func (p *PublicParams) SetIssuers(ids []driver.Identity) {
+	p.IssuerIDs = ids
+}
+
+// SetAuditors sets the auditors to the passed identities
+func (p *PublicParams) SetAuditors(ids []driver.Identity) {
+	p.Auditor = ids[0]
 }
 
 // Auditors returns the list of authorized auditors
 // fabtoken only supports a single auditor
-func (pp *PublicParams) Auditors() []driver.Identity {
-	if len(pp.Auditor) == 0 {
+func (p *PublicParams) Auditors() []driver.Identity {
+	if len(p.Auditor) == 0 {
 		return []driver.Identity{}
 	}
-	return []driver.Identity{pp.Auditor}
+	return []driver.Identity{p.Auditor}
 }
 
 // Issuers returns the list of authorized issuers
-func (pp *PublicParams) Issuers() []driver.Identity {
-	return pp.IssuerIDs
+func (p *PublicParams) Issuers() []driver.Identity {
+	return p.IssuerIDs
 }
 
 // Precision returns the quantity precision encoded in PublicParams
-func (pp *PublicParams) Precision() uint64 {
-	return pp.QuantityPrecision
+func (p *PublicParams) Precision() uint64 {
+	return p.QuantityPrecision
 }
 
 // Validate validates the public parameters
-func (pp *PublicParams) Validate() error {
-	if pp.QuantityPrecision > 64 {
-		return errors.Errorf("invalid precision [%d], must be less than 64", pp.QuantityPrecision)
+func (p *PublicParams) Validate() error {
+	if p.QuantityPrecision > 64 {
+		return errors.Errorf("invalid precision [%d], must be less than 64", p.QuantityPrecision)
 	}
-	if pp.QuantityPrecision == 0 {
+	if p.QuantityPrecision == 0 {
 		return errors.New("invalid precision, must be greater than 0")
 	}
-	maxTokenValue := uint64(1<<pp.Precision()) - 1
-	if pp.MaxToken > maxTokenValue {
-		return errors.Errorf("max token value is invalid [%d]>[%d]", pp.MaxToken, maxTokenValue)
+	maxTokenValue := uint64(1<<p.Precision()) - 1
+	if p.MaxToken > maxTokenValue {
+		return errors.Errorf("max token value is invalid [%d]>[%d]", p.MaxToken, maxTokenValue)
 	}
 	return nil
 }
 
-func (pp *PublicParams) String() string {
-	res, err := json.MarshalIndent(pp, " ", "  ")
+func (p *PublicParams) String() string {
+	res, err := json.MarshalIndent(p, " ", "  ")
 	if err != nil {
 		return err.Error()
 	}
