3dffa3c7c60848d637f4bb4aa7967e743125c6bc
Angelo De Caro <angelo.decaro@gmail.com>
2025-03-27 06:15:34 +0100

    issue action
    Signed-off-by: Angelo De Caro <angelo.decaro@gmail.com>

--- token/core/fabtoken/v1/actions/issue.go
+++ token/core/fabtoken/v1/actions/issue.go
@@ -7,8 +7,11 @@ SPDX-License-Identifier: Apache-2.0
 package actions
 
 import (
-	"github.com/hyperledger-labs/fabric-token-sdk/token/core/common/encoding/json"
+	"github.com/hyperledger-labs/fabric-smart-client/pkg/utils/proto"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/core/fabtoken/protos-go/actions"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/core/fabtoken/protos-go/pp"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/driver"
+	"github.com/hyperledger-labs/fabric-token-sdk/token/services/utils/protos"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/token"
 	"github.com/pkg/errors"
 )
@@ -39,14 +42,65 @@ func (i *IssueAction) GetSerialNumbers() []string {
 	return nil
 }
 
-// Serialize marshals IssueAction
+// Serialize marshal IssueAction
 func (i *IssueAction) Serialize() ([]byte, error) {
-	return json.Marshal(i)
+	// outputs
+	outputs, err := protos.ToProtosSliceFunc(i.Outputs, func(output *Output) (*actions.IssueActionOutput, error) {
+		return &actions.IssueActionOutput{
+			Token: &actions.Token{
+				Owner:    output.Owner,
+				Type:     string(output.Type),
+				Quantity: output.Quantity,
+			},
+		}, nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to serialize outputs")
+	}
+
+	issueAction := &actions.IssueAction{
+		Version: ProtocolV1,
+		Issuer: &pp.Identity{
+			Raw: i.Issuer,
+		},
+		Outputs:  outputs,
+		Metadata: i.Metadata,
+	}
+	return proto.Marshal(issueAction)
 }
 
 // Deserialize un-marshals IssueAction
 func (i *IssueAction) Deserialize(raw []byte) error {
-	return json.Unmarshal(raw, i)
+	issueAction := &actions.IssueAction{}
+	err := proto.Unmarshal(raw, issueAction)
+	if err != nil {
+		return errors.Wrap(err, "failed to deserialize issue action")
+	}
+
+	// assert version
+	if issueAction.Version != ProtocolV1 {
+		return errors.Errorf("invalid issue version, expected [%d], got [%d]", ProtocolV1, issueAction.Version)
+	}
+
+	// outputs
+	i.Outputs = make([]*Output, len(issueAction.Outputs))
+	for j, output := range issueAction.Outputs {
+		if output == nil || output.Token == nil {
+			continue
+		}
+		i.Outputs[j] = &Output{
+			Owner:    output.Token.Owner,
+			Type:     token.Type(output.Token.Type),
+			Quantity: output.Token.Quantity,
+		}
+	}
+
+	if issueAction.Issuer != nil {
+		i.Issuer = issueAction.Issuer.Raw
+	}
+	i.Metadata = issueAction.Metadata
+
+	return nil
 }
 
 // NumOutputs returns the number of outputs in an IssueAction
@@ -107,9 +161,15 @@ func (i *IssueAction) Validate() error {
 	if len(i.Outputs) == 0 {
 		return errors.Errorf("no outputs in issue action")
 	}
-	for _, output := range i.Outputs {
+	for i, output := range i.Outputs {
 		if output == nil {
 			return errors.Errorf("nil output in issue action")
+		}
+		if len(output.Type) == 0 {
+			return errors.Errorf("invalid output's type at index [%d], output type is empty", i)
+		}
+		if len(output.Quantity) == 0 {
+			return errors.Errorf("invalid output's quantity at index [%d], output quantity is empty", i)
 		}
 	}
 	return nil
