aa4d452827b3891a918a79f466d3c06ff3a45653
bar haim <barha@il.ibm.com>
2025-03-31 17:44:49 +0300

    redeem: fabtoken issuer in action
    Signed-off-by: bar haim <barha@il.ibm.com>

--- integration/token/fungible/views/redeem.go
+++ integration/token/fungible/views/redeem.go
@@ -8,7 +8,6 @@ package views
 
 import (
 	"encoding/json"
-
 	view2 "github.com/hyperledger-labs/fabric-smart-client/platform/view"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/assert"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
