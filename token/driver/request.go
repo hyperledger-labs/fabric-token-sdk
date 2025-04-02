3b34adf042faafa4a0ac91ebf68df7dac75ada01
bar haim <barha@il.ibm.com>
2025-04-02 22:08:06 +0300

    redeem: issuer in transfer metadata proto
    Signed-off-by: bar haim <barha@il.ibm.com>

--- token/driver/request.go
+++ token/driver/request.go
@@ -359,10 +359,18 @@ func (t *TransferMetadata) ToProtos() (*request.TransferMetadata, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed marshalling outputs")
 	}
+
+	var issuer *request.Identity
+	if t.Issuer != nil {
+		issuer = &request.Identity{
+			Raw: t.Issuer.Bytes(),
+		}
+	}
 	return &request.TransferMetadata{
 		Inputs:       inputs,
 		Outputs:      outputs,
 		ExtraSigners: ToProtoIdentitySlice(t.ExtraSigners),
+		Issuer:       issuer,
 	}, nil
 }
 
@@ -376,6 +384,7 @@ func (t *TransferMetadata) FromProtos(transferMetadata *request.TransferMetadata
 		return errors.Wrap(err, "failed unmarshalling outputs")
 	}
 	t.ExtraSigners = FromProtoIdentitySlice(transferMetadata.ExtraSigners)
+	t.Issuer = transferMetadata.Issuer.Raw
 	return nil
 }
 
