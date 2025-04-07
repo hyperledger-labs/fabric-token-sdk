06c3a424ce06ec306366a2568a029789a25a4832
bar haim <barha@il.ibm.com>
2025-04-07 11:49:07 +0300

    redeem: make checks
    Signed-off-by: bar haim <barha@il.ibm.com>
    Signed-off-by: Allon Adir <adir@il.ibm.com>

--- integration/token/fungible/views/redeem.go
+++ integration/token/fungible/views/redeem.go
@@ -8,6 +8,7 @@ package views
 
 import (
 	"encoding/json"
+
 	view2 "github.com/hyperledger-labs/fabric-smart-client/platform/view"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/assert"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
