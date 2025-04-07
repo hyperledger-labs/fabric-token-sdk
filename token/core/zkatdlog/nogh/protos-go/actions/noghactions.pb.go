591eae66fdbdb2d67535f07989cf9ec7819d706c
bar haim <barha@il.ibm.com>
2025-04-07 11:59:28 +0300

    redeem: rebase
    Signed-off-by: bar haim <barha@il.ibm.com>
    Signed-off-by: Allon Adir <adir@il.ibm.com>

--- token/core/zkatdlog/nogh/protos-go/actions/noghactions.pb.go
+++ token/core/zkatdlog/nogh/protos-go/actions/noghactions.pb.go
@@ -12,13 +12,14 @@
 package actions
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	actions "github.com/hyperledger-labs/fabric-token-sdk/token/core/fabtoken/protos-go/actions"
 	math "github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/nogh/protos-go/math"
 	pp "github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/nogh/protos-go/pp"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
