efa6e2db06db9d865bcd4e8f988391740e4c194f
Angelo De Caro <angelo.decaro@gmail.com>
2025-04-18 16:04:30 +0200

    introduce Validate to the proofs
    Signed-off-by: Angelo De Caro <angelo.decaro@gmail.com>

--- token/core/zkatdlog/nogh/v1/crypto/math/math.go
+++ token/core/zkatdlog/nogh/v1/crypto/math/math.go
@@ -13,9 +13,13 @@ import (
 	"github.com/hyperledger-labs/fabric-smart-client/pkg/utils/errors"
 )
 
-type Element interface {
-	IsInfinity() bool
+type BaseElement interface {
 	CurveID() mathlib.CurveID
+}
+
+type Element interface {
+	BaseElement
+	IsInfinity() bool
 }
 
 func CheckElements[E Element](elements []E, curveID mathlib.CurveID, length uint64) error {
@@ -24,6 +28,18 @@ func CheckElements[E Element](elements []E, curveID mathlib.CurveID, length uint
 	}
 	for _, g1 := range elements {
 		if err := CheckElement[E](g1, curveID); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func CheckZrElements[E BaseElement](elements []E, curveID mathlib.CurveID, length uint64) error {
+	if uint64(len(elements)) != length {
+		return errors.Errorf("length of elements does not match length of curveID")
+	}
+	for _, g1 := range elements {
+		if err := CheckBaseElement[E](g1, curveID); err != nil {
 			return err
 		}
 	}
@@ -45,6 +61,22 @@ func CheckElement[E Element](element E, curveID mathlib.CurveID) (err error) {
 	}
 	if element.IsInfinity() {
 		return errors.New("element is infinity")
+	}
+	return nil
+}
+
+func CheckBaseElement[E BaseElement](element E, curveID mathlib.CurveID) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = errors.Errorf("caught panic while checking element, err [%s]", e)
+		}
+	}()
+
+	if isNilInterface(element) {
+		return errors.Errorf("elememt is nil")
+	}
+	if element.CurveID() != curveID {
+		return errors.Errorf("element curve must equal curve ID")
 	}
 	return nil
 }
