37dbcbddd131e8578257a916d25e2bce6c9b55a3
Angelo De Caro <angelo.decaro@gmail.com>
2025-04-18 09:34:46 +0200

    missing licence
    Signed-off-by: Angelo De Caro <angelo.decaro@gmail.com>

--- token/services/utils/closer.go
+++ token/services/utils/closer.go
@@ -1,3 +1,9 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package utils
 
 // IgnoreError runs a function that returns an error and silently ignores the error.
