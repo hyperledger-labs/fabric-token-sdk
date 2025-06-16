9d0d86b78e10e964177340581d107ba2dd38ade9
ADIR <adir@il.ibm.com>
2025-06-16 14:55:04 +0300

    test: transaction queries
    Signed-off-by: ADIR <adir@il.ibm.com>

--- token/services/db/sql/postgres/transactions_test.go
+++ token/services/db/sql/postgres/transactions_test.go
@@ -7,18 +7,11 @@ SPDX-License-Identifier: Apache-2.0
 package postgres
 
 import (
-	"context"
 	"database/sql"
-	driver2 "database/sql/driver"
-	"math/big"
 	"testing"
-	"time"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services/db/driver/sql/sqlite"
-	"github.com/hyperledger-labs/fabric-token-sdk/token/services/db/driver"
 	"github.com/hyperledger-labs/fabric-token-sdk/token/services/db/sql/common"
-	"github.com/onsi/gomega"
 )
 
 func mockTransactionsStore(db *sql.DB) *common.TransactionStore {
@@ -30,44 +23,6 @@ func mockTransactionsStore(db *sql.DB) *common.TransactionStore {
 		TransactionEndorseAck: "TRANSACTION_ENDORSE_ACK",
 	}, sqlite.NewConditionInterpreter(), sqlite.NewPaginationInterpreter())
 	return store
-}
-
-type AnyUUID struct{}
-
-func (a AnyUUID) Match(v driver2.Value) bool {
-	_, ok := v.(string)
-	return ok
-}
-
-func TestAddTransaction(t *testing.T) {
-	gomega.RegisterTestingT(t)
-	db, mockDB, err := sqlmock.New()
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-
-	input := &driver.TransactionRecord{
-		TxID:         "txid",
-		ActionType:   driver.Transfer,
-		SenderEID:    "sender",
-		RecipientEID: "recipient",
-		TokenType:    "USD",
-		Amount:       big.NewInt(10),
-		Timestamp:    time.Now(),
-	}
-
-	mockDB.ExpectBegin()
-	mockDB.
-		ExpectExec("INSERT INTO TRANSACTIONS \\(id, tx_id, action_type, sender_eid, recipient_eid, token_type, amount, stored_at\\) VALUES \\(\\$1, \\$2, \\$3, \\$4, \\$5, \\$6, \\$7, \\$8\\)").
-		WithArgs(AnyUUID{}, input.TxID, 1, input.SenderEID, input.RecipientEID, input.TokenType, input.Amount.Int64(), input.Timestamp.UTC()).
-		WillReturnResult(sqlmock.NewResult(0, 1))
-	mockDB.ExpectCommit()
-
-	aw, err := mockTransactionsStore(db).BeginAtomicWrite()
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	gomega.Expect(aw.AddTransaction(context.Background(), input)).To(gomega.Succeed())
-	gomega.Expect(aw.Commit()).To(gomega.Succeed())
-
-	gomega.Expect(mockDB.ExpectationsWereMet()).To(gomega.Succeed())
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 }
 
 func TestGetTokenRequest(t *testing.T) {
@@ -104,4 +59,20 @@ func TestAddTransactionEndorsementAck(t *testing.T) {
 
 func TestSetStatus(t *testing.T) {
 	common.TestSetStatus(t, mockTransactionsStore)
+}
+
+func TestAWAddTransaction(t *testing.T) {
+	common.TestAWAddTransaction(t, mockTransactionsStore)
+}
+
+func TestAWAddTokenRequest(t *testing.T) {
+	common.TestAWAddTokenRequest(t, mockTransactionsStore)
+}
+
+func TestAWAddMovement(t *testing.T) {
+	common.TestAWAddMovement(t, mockTransactionsStore)
+}
+
+func TestAWAddValidationRecord(t *testing.T) {
+	common.TestAWAddValidationRecord(t, mockTransactionsStore)
 }
