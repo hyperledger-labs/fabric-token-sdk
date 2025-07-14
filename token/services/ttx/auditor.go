fbc3b559d2e4c5280c16e723216da057500a9596
Angelo De Caro <adc@zurich.ibm.com>
2025-07-14 10:51:24 +0200

    context logger: ttx/auditor.go
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/ttx/auditor.go
+++ token/services/ttx/auditor.go
@@ -248,7 +248,7 @@ func (a *AuditingViewInitiator) verifyAuditorSignature(context view.Context, sig
 	for _, auditorID := range a.tx.TokenService().PublicParametersManager().PublicParameters().Auditors() {
 		v, err := a.tx.TokenService().SigService().AuditorVerifier(auditorID)
 		if err != nil {
-			logger.Debugf("failed to get auditor verifier for [%s]", auditorID)
+			logger.DebugfContext(context.Context(), "failed to get auditor verifier for [%s]", auditorID)
 			continue
 		}
 		logger.DebugfContext(context.Context(), "Verify auditor signature")
@@ -343,7 +343,7 @@ func (a *AuditApproveView) waitEnvelope(context view.Context) error {
 	logger.DebugfContext(context.Context(), "Waiting for envelope...transaction received[%s]", a.tx.ID())
 
 	// Processes
-	logger.Debugf("Processes envelope...")
+	logger.DebugfContext(context.Context(), "Processes envelope...")
 	if tx.Payload == nil {
 		return errors.Errorf("expected transaction payload not found")
 	}
