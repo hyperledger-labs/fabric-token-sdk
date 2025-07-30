bde1d466a7e1f66025cf04a20fe8d6ec01dfaf5e
Angelo De Caro <adc@zurich.ibm.com>
2025-07-30 16:57:59 +0200

    introduce nocache
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ token/services/utils/cache/nocache.go
@@ -0,0 +1,30 @@
+package cache
+
+type NoCache[T any] struct {
+}
+
+func NewNoCache[T any]() *NoCache[T] {
+	return &NoCache[T]{}
+}
+
+func (n *NoCache[T]) Get(key string) (T, bool) {
+	return Zero[T](), false
+}
+
+func (n *NoCache[T]) GetOrLoad(key string, loader func() (T, error)) (T, bool, error) {
+	v, err := loader()
+	return v, false, err
+}
+
+func (n *NoCache[T]) Add(key string, value T) {
+	return
+}
+
+func (n *NoCache[T]) Delete(key string) {
+	return
+}
+
+func Zero[T any]() T {
+	var result T
+	return result
+}
