73895076809c133651411fbf19953a41d6c2626b
Angelo De Caro <adc@zurich.ibm.com>
2025-08-06 08:30:06 +0200

    merge sig service into the identity service
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/identity/provider.go
+++ token/services/identity/provider.go
@@ -35,16 +35,6 @@ type enrollmentIDUnmarshaler interface {
 	GetEIDAndRH(ctx context.Context, identity driver.Identity, auditInfo []byte) (string, string, error)
 }
 
-type sigService interface {
-	IsMe(ctx context.Context, identity driver.Identity) bool
-	AreMe(ctx context.Context, identities ...driver.Identity) []string
-	RegisterSigner(ctx context.Context, identity driver.Identity, signer driver.Signer, verifier driver.Verifier, signerInfo []byte) error
-	RegisterVerifier(ctx context.Context, identity driver.Identity, v driver.Verifier) error
-	GetSigner(ctx context.Context, identity driver.Identity) (driver.Signer, error)
-	GetSignerInfo(ctx context.Context, identity driver.Identity) ([]byte, error)
-	GetVerifier(ctx context.Context, identity driver.Identity) (driver.Verifier, error)
-}
-
 type storage interface {
 	GetAuditInfo(ctx context.Context, id []byte) ([]byte, error)
 	StoreIdentityData(ctx context.Context, id []byte, identityAudit []byte, tokenMetadata []byte, tokenMetadataAudit []byte) error
@@ -59,16 +49,32 @@ type cache[T any] interface {
 	Add(key string, value T)
 }
 
+type VerifierEntry struct {
+	Verifier   driver.Verifier
+	DebugStack []byte
+}
+
+type SignerEntry struct {
+	Signer     driver.Signer
+	DebugStack []byte
+}
+
 // Provider implements the driver.IdentityProvider interface.
 // Provider handles the long-term identities on top of which wallets are defined.
 type Provider struct {
-	Logger     logging.Logger
-	SigService sigService
-	Binder     idriver.NetworkBinderService
-	Storage    storage
+	Logger  logging.Logger
+	Binder  idriver.NetworkBinderService
+	Storage storage
 
 	enrollmentIDUnmarshaler enrollmentIDUnmarshaler
 	isMeCache               cache[bool]
+
+	sync      sync.RWMutex
+	signers   map[string]SignerEntry
+	verifiers map[string]VerifierEntry
+
+	storage      storage
+	deserializer idriver.Deserializer
 }
 
 // NewProvider creates a new identity provider implementing the driver.IdentityProvider interface.
@@ -83,10 +89,14 @@ func NewProvider(
 	return &Provider{
 		Logger:                  logger,
 		Storage:                 storage,
-		SigService:              NewService(deserializer, storage),
 		Binder:                  binder,
 		enrollmentIDUnmarshaler: enrollmentIDUnmarshaler,
 		isMeCache:               secondcache.NewTyped[bool](1000),
+
+		signers:      map[string]SignerEntry{},
+		verifiers:    map[string]VerifierEntry{},
+		deserializer: deserializer,
+		storage:      storage,
 	}
 }
 
@@ -112,7 +122,7 @@ func (p *Provider) RegisterIdentityDescriptor(ctx context.Context, identityDescr
 }
 
 func (p *Provider) RegisterVerifier(ctx context.Context, identity driver.Identity, v driver.Verifier) error {
-	return p.SigService.RegisterVerifier(ctx, identity, v)
+	return p.localRegisterVerifier(ctx, identity, v)
 }
 
 func (p *Provider) RegisterAuditInfo(ctx context.Context, identity driver.Identity, info []byte) error {
@@ -131,7 +141,7 @@ func (p *Provider) RegisterSigner(ctx context.Context, identity driver.Identity,
 	defer func() {
 		p.isMeCache.Add(identity.UniqueID(), true)
 	}()
-	return p.SigService.RegisterSigner(ctx, identity, signer, verifier, signerInfo)
+	return p.localRegisterSigner(ctx, identity, signer, verifier, signerInfo)
 }
 
 func (p *Provider) AreMe(ctx context.Context, identities ...driver.Identity) []string {
@@ -152,7 +162,7 @@ func (p *Provider) AreMe(ctx context.Context, identities ...driver.Identity) []s
 		return result
 	}
 
-	found := p.SigService.AreMe(ctx, notFound...)
+	found := p.localAreMe(ctx, notFound...)
 	for _, id := range notFound {
 		uniqueID := id.UniqueID()
 		p.isMeCache.Add(uniqueID, slices.Contains(found, uniqueID))
@@ -175,7 +185,7 @@ func (p *Provider) GetSigner(ctx context.Context, identity driver.Identity) (dri
 	defer func() {
 		p.isMeCache.Add(identity.UniqueID(), found)
 	}()
-	signer, err := p.SigService.GetSigner(ctx, identity)
+	signer, err := p.localGetSigner(ctx, identity)
 	if err != nil {
 		p.Logger.Warn(err)
 		return nil, errors.Errorf("failed to get signer for identity [%s], it is neither register nor deserialazable", identity.String())
@@ -211,7 +221,7 @@ func (p *Provider) Copy(ctx context.Context, longTerm driver.Identity, ephemeral
 		}
 		setSV = false
 	}
-	verifier, err := p.SigService.GetVerifier(ctx, longTerm)
+	verifier, err := p.localGetVerifier(ctx, longTerm)
 	if err != nil {
 		if p.Logger.IsEnabledFor(zapcore.DebugLevel) {
 			p.Logger.DebugfContext(ctx, "failed getting verifier for identity [%s][%s][%s]", longTerm, err, string(debug.Stack()))
@@ -227,11 +237,11 @@ func (p *Provider) Copy(ctx context.Context, longTerm driver.Identity, ephemeral
 	}
 
 	if setSV {
-		signerInfo, err := p.SigService.GetSignerInfo(ctx, longTerm)
+		signerInfo, err := p.localGetSignerInfo(ctx, longTerm)
 		if err != nil {
 			return err
 		}
-		if err := p.SigService.RegisterSigner(ctx, ephemeral, signer, verifier, signerInfo); err != nil {
+		if err := p.localRegisterSigner(ctx, ephemeral, signer, verifier, signerInfo); err != nil {
 			return err
 		}
 	}
@@ -256,47 +266,19 @@ func (p *Provider) Bind(ctx context.Context, longTerm driver.Identity, ephemeral
 	return nil
 }
 
-type VerifierEntry struct {
-	Verifier   driver.Verifier
-	DebugStack []byte
-}
-
-type SignerEntry struct {
-	Signer     driver.Signer
-	DebugStack []byte
-}
-
-type Service struct {
-	sync      sync.RWMutex
-	signers   map[string]SignerEntry
-	verifiers map[string]VerifierEntry
-
-	storage      storage
-	deserializer idriver.Deserializer
-}
-
-func NewService(deserializer idriver.Deserializer, storage storage) *Service {
-	return &Service{
-		signers:      map[string]SignerEntry{},
-		verifiers:    map[string]VerifierEntry{},
-		deserializer: deserializer,
-		storage:      storage,
-	}
-}
-
-func (o *Service) RegisterSigner(ctx context.Context, identity driver.Identity, signer driver.Signer, verifier driver.Verifier, signerInfo []byte) error {
+func (p *Provider) localRegisterSigner(ctx context.Context, identity driver.Identity, signer driver.Signer, verifier driver.Verifier, signerInfo []byte) error {
 	idHash := identity.UniqueID()
 	logger.DebugfContext(ctx, "register signer and verifier [%s]:[%s][%s]", idHash, logging.Identifier(signer), logging.Identifier(verifier))
 
 	if signer != nil {
-		if err := o.registerSigner(ctx, identity, signer, verifier, signerInfo); err != nil {
+		if err := p.registerSigner(ctx, identity, signer, verifier, signerInfo); err != nil {
 			return err
 		}
 	}
 
 	if verifier != nil {
-		if err := o.RegisterVerifier(ctx, identity, verifier); err != nil {
-			o.deleteSigner(idHash)
+		if err := p.RegisterVerifier(ctx, identity, verifier); err != nil {
+			p.deleteSigner(idHash)
 			return err
 		}
 	}
@@ -305,28 +287,28 @@ func (o *Service) RegisterSigner(ctx context.Context, identity driver.Identity, 
 	return nil
 }
 
-func (o *Service) RegisterVerifier(ctx context.Context, identity driver.Identity, verifier driver.Verifier) error {
+func (p *Provider) localRegisterVerifier(ctx context.Context, identity driver.Identity, verifier driver.Verifier) error {
 	if verifier == nil {
 		return errors.New("invalid verifier, expected a valid instance")
 	}
 
 	// First check with read lock
 	idHash := identity.UniqueID()
-	o.sync.RLock()
-	v, ok := o.verifiers[idHash]
-	o.sync.RUnlock()
+	p.sync.RLock()
+	v, ok := p.verifiers[idHash]
+	p.sync.RUnlock()
 	if ok {
 		logger.Warnf("another verifier bound to [%s]:[%s][%s] from [%s]", idHash, logging.Identifier(v), logging.Identifier(verifier), string(v.DebugStack))
 		return nil
 	}
 
 	// write lock
-	o.sync.Lock()
+	p.sync.Lock()
 
 	// check again
-	v, ok = o.verifiers[idHash]
+	v, ok = p.verifiers[idHash]
 	if ok {
-		o.sync.Unlock()
+		p.sync.Unlock()
 		logger.Warnf("another verifier bound to [%s]:[%s][%s] from [%s]", idHash, logging.Identifier(v), logging.Identifier(verifier), string(v.DebugStack))
 		return nil
 	}
@@ -335,14 +317,14 @@ func (o *Service) RegisterVerifier(ctx context.Context, identity driver.Identity
 	if logger.IsEnabledFor(zapcore.DebugLevel) {
 		entry.DebugStack = debug.Stack()
 	}
-	o.verifiers[idHash] = entry
-	o.sync.Unlock()
+	p.verifiers[idHash] = entry
+	p.sync.Unlock()
 
 	logger.DebugfContext(ctx, "register verifier to [%s]:[%s]", idHash, logging.Identifier(verifier))
 	return nil
 }
 
-func (o *Service) AreMe(ctx context.Context, identities ...driver.Identity) []string {
+func (p *Provider) localAreMe(ctx context.Context, identities ...driver.Identity) []string {
 	logger.DebugfContext(ctx, "is me [%s]?", identities)
 	idHashes := make([]string, len(identities))
 	for i, id := range identities {
@@ -353,23 +335,23 @@ func (o *Service) AreMe(ctx context.Context, identities ...driver.Identity) []st
 	notFound := make([]driver.Identity, 0)
 
 	// check local cache
-	o.sync.RLock()
+	p.sync.RLock()
 	for _, id := range identities {
-		if _, ok := o.signers[id.UniqueID()]; ok {
+		if _, ok := p.signers[id.UniqueID()]; ok {
 			logger.DebugfContext(ctx, "is me [%s]? yes, from cache", id)
 			result.Add(id.UniqueID())
 		} else {
 			notFound = append(notFound, id)
 		}
 	}
-	o.sync.RUnlock()
+	p.sync.RUnlock()
 
 	if len(notFound) == 0 {
 		return result.ToSlice()
 	}
 
 	// check storage
-	found, err := o.storage.GetExistingSignerInfo(ctx, notFound...)
+	found, err := p.storage.GetExistingSignerInfo(ctx, notFound...)
 	if err != nil {
 		logger.Errorf("failed checking if a signer exists [%s]", err)
 		return result.ToSlice()
@@ -378,79 +360,53 @@ func (o *Service) AreMe(ctx context.Context, identities ...driver.Identity) []st
 	return result.ToSlice()
 }
 
-func (o *Service) IsMe(ctx context.Context, identity driver.Identity) bool {
-	logger.DebugfContext(ctx, "is me [%s]?", identity)
-	idHash := identity.UniqueID()
-
-	// check local cache
-	o.sync.RLock()
-	_, ok := o.signers[idHash]
-	o.sync.RUnlock()
-	if ok {
-		logger.DebugfContext(ctx, "is me [%s]? yes, from cache", identity)
-		return true
-	}
-
-	// check storage
-	logger.DebugfContext(ctx, "is me [%s]? ask the storage", identity)
-	exists, err := o.storage.SignerInfoExists(ctx, identity)
-	if err != nil {
-		logger.Errorf("failed checking if a signer exists [%s]", err)
-	}
-	if exists {
-		logger.DebugfContext(ctx, "is me [%s]? yes, from storage", identity)
-		return true
-	}
-	return false
-}
-
-func (o *Service) GetSigner(ctx context.Context, identity driver.Identity) (driver.Signer, error) {
+func (p *Provider) localGetSigner(ctx context.Context, identity driver.Identity) (driver.Signer, error) {
 	idHash := identity.UniqueID()
 	logger.DebugfContext(ctx, "get signer for [%s]", idHash)
 	// check the cache
-	o.sync.RLock()
-	entry, ok := o.signers[idHash]
-	o.sync.RUnlock()
+	p.sync.RLock()
+	entry, ok := p.signers[idHash]
+	p.sync.RUnlock()
 	if ok {
 		logger.DebugfContext(ctx, "signer for [%s] found", idHash)
 		return entry.Signer, nil
 	}
-	o.sync.Lock()
-	defer o.sync.Unlock()
+	p.sync.Lock()
+	defer p.sync.Unlock()
 
-	return o.getSigner(ctx, identity, idHash)
+	return p.getSigner(ctx, identity, idHash)
 }
 
-func (o *Service) GetSignerInfo(ctx context.Context, identity driver.Identity) ([]byte, error) {
-	return o.storage.GetSignerInfo(ctx, identity)
+func (p *Provider) localGetSignerInfo(ctx context.Context, identity driver.Identity) ([]byte, error) {
+	return p.storage.GetSignerInfo(ctx, identity)
 }
 
-func (o *Service) GetVerifier(ctx context.Context, identity driver.Identity) (driver.Verifier, error) {
+func (p *Provider) localGetVerifier(ctx context.Context, identity driver.Identity) (driver.Verifier, error) {
 	idHash := identity.UniqueID()
 
 	// check cache
-	o.sync.RLock()
-	entry, ok := o.verifiers[idHash]
-	o.sync.RUnlock()
+	p.sync.RLock()
+	entry, ok := p.verifiers[idHash]
+	p.sync.RUnlock()
 	if ok {
 		return entry.Verifier, nil
 	}
 
-	o.sync.Lock()
-	defer o.sync.Unlock()
+	p.sync.Lock()
+	defer p.sync.Unlock()
 
 	// check cache again
-	entry, ok = o.verifiers[idHash]
+	entry, ok = p.verifiers[idHash]
 	if ok {
 		return entry.Verifier, nil
 	}
 
 	// ask the deserializer
-	if o.deserializer == nil {
+	if p.deserializer == nil {
 		return nil, errors.Errorf("cannot find verifier for [%s], no deserializer set", identity)
 	}
 	var err error
-	verifier, err := o.deserializer.DeserializeVerifier(ctx, identity)
+	verifier, err := p.deserializer.DeserializeVerifier(ctx, identity)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed deserializing identity for verifier %v", identity)
 	}
@@ -461,11 +417,11 @@ func (o *Service) GetVerifier(ctx context.Context, identity driver.Identity) (dr
 		entry.DebugStack = debug.Stack()
 	}
 	logger.DebugfContext(ctx, "add deserialized verifier for [%s]:[%s]", idHash, logging.Identifier(verifier))
-	o.verifiers[idHash] = entry
+	p.verifiers[idHash] = entry
 	return verifier, nil
 }
 
-func (o *Service) registerSigner(ctx context.Context, identity driver.Identity, signer driver.Signer, verifier driver.Verifier, signerInfo []byte) error {
+func (p *Provider) registerSigner(ctx context.Context, identity driver.Identity, signer driver.Signer, verifier driver.Verifier, signerInfo []byte) error {
 	if signer == nil {
 		// Nothing to do here
 		return nil
@@ -474,21 +430,21 @@ func (o *Service) registerSigner(ctx context.Context, identity driver.Identity, 
 	idHash := identity.UniqueID()
 	logger.DebugfContext(ctx, "register signer and verifier [%s]:[%s][%s]", idHash, logging.Identifier(signer), logging.Identifier(verifier))
 	// First check with read lock
-	o.sync.RLock()
-	s, ok := o.signers[idHash]
-	o.sync.RUnlock()
+	p.sync.RLock()
+	s, ok := p.signers[idHash]
+	p.sync.RUnlock()
 	if ok {
 		logger.Warnf("another signer bound to [%s]:[%s][%s] from [%s]", identity, logging.Identifier(s), logging.Identifier(signer), string(s.DebugStack))
 		return nil
 	}
 
 	// write lock
-	o.sync.Lock()
+	p.sync.Lock()
 
 	// check again the cache
-	s, ok = o.signers[idHash]
+	s, ok = p.signers[idHash]
 	if ok {
-		o.sync.Unlock()
+		p.sync.Unlock()
 		logger.Warnf("another signer bound to [%s]:[%s][%s] from [%s]", identity, logging.Identifier(s), logging.Identifier(signer), string(s.DebugStack))
 		return nil
 	}
@@ -497,22 +453,22 @@ func (o *Service) registerSigner(ctx context.Context, identity driver.Identity, 
 	if logger.IsEnabledFor(zapcore.DebugLevel) {
 		entry.DebugStack = debug.Stack()
 	}
-	o.signers[idHash] = entry
-	o.sync.Unlock()
+	p.signers[idHash] = entry
+	p.sync.Unlock()
 
 	// store, if a failure happens then remove the entry
 	logger.DebugfContext(ctx, "checks done, store signer info")
-	if err := o.storage.StoreSignerInfo(ctx, identity, signerInfo); err != nil {
-		o.deleteSigner(idHash)
+	if err := p.storage.StoreSignerInfo(ctx, identity, signerInfo); err != nil {
+		p.deleteSigner(idHash)
 		return errors.Wrap(err, "failed to store entry in storage for the passed signer")
 	}
 
 	return nil
 }
 
-func (o *Service) getSigner(ctx context.Context, identity driver.Identity, idHash string) (driver.Signer, error) {
+func (p *Provider) getSigner(ctx context.Context, identity driver.Identity, idHash string) (driver.Signer, error) {
 	// check again the cache
-	entry, ok := o.signers[idHash]
+	entry, ok := p.signers[idHash]
 	if ok {
 		logger.DebugfContext(ctx, "signer for [%s] found", idHash)
 		return entry.Signer, nil
@@ -520,7 +476,7 @@ func (o *Service) getSigner(ctx context.Context, identity driver.Identity, idHas
 
 	logger.DebugfContext(ctx, "signer for [%s] not found, try to deserialize", idHash)
 	// ask the deserializer
-	signer, err := o.deserializeSigner(ctx, identity)
+	signer, err := p.deserializeSigner(ctx, identity)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed deserializing identity for signer [%s]", identity)
 	}
@@ -528,19 +484,19 @@ func (o *Service) getSigner(ctx context.Context, identity driver.Identity, idHas
 	if logger.IsEnabledFor(zapcore.DebugLevel) {
 		entry.DebugStack = debug.Stack()
 	}
-	o.signers[idHash] = entry
-	if err := o.storage.StoreSignerInfo(ctx, identity, nil); err != nil {
+	p.signers[idHash] = entry
+	if err := p.storage.StoreSignerInfo(ctx, identity, nil); err != nil {
 		return nil, errors.Wrap(err, "failed to store entry in storage for the passed signer")
 	}
 	return entry.Signer, nil
 }
 
-func (o *Service) deserializeSigner(ctx context.Context, identity driver.Identity) (driver.Signer, error) {
-	if o.deserializer == nil {
+func (p *Provider) deserializeSigner(ctx context.Context, identity driver.Identity) (driver.Signer, error) {
+	if p.deserializer == nil {
 		return nil, errors.Errorf("cannot find signer for [%s], no deserializer set", identity)
 	}
 	var err error
-	signer, err := o.deserializer.DeserializeSigner(ctx, identity)
+	signer, err := p.deserializer.DeserializeSigner(ctx, identity)
 	if err == nil {
 		return signer, nil
 	}
@@ -555,15 +511,15 @@ func (o *Service) deserializeSigner(ctx context.Context, identity driver.Identit
 	}
 
 	// yes, check ro.Identity
-	signer, err = o.getSigner(ctx, ro.Identity, ro.Identity.UniqueID())
+	signer, err = p.getSigner(ctx, ro.Identity, ro.Identity.UniqueID())
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed getting signer for identity [%s]", ro.Identity)
 	}
 	return signer, nil
 }
 
-func (o *Service) deleteSigner(id string) {
-	o.sync.Lock()
-	defer o.sync.Unlock()
-	delete(o.signers, id)
+func (p *Provider) deleteSigner(id string) {
+	p.sync.Lock()
+	defer p.sync.Unlock()
+	delete(p.signers, id)
 }
