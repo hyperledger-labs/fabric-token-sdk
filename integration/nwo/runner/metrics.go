7f19e3739b24d45d767d28e12e2d3c74625f6483
Angelo De Caro <adc@zurich.ibm.com>
2025-08-09 07:04:18 +0200

    fsc 29b3f682f926a3926940968382febaad4aeaece1
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- integration/nwo/runner/metrics.go
+++ integration/nwo/runner/metrics.go
@@ -21,38 +21,35 @@ type provider struct {
 
 func (p *provider) NewCounter(opts metrics.CounterOpts) metrics.Counter {
 	return &counter{p.p.NewCounter(metrics2.CounterOpts{
-		Namespace:    opts.Namespace,
-		Subsystem:    opts.Subsystem,
-		Name:         opts.Name,
-		Help:         opts.Help,
-		LabelNames:   opts.LabelNames,
-		LabelHelp:    opts.LabelHelp,
-		StatsdFormat: opts.StatsdFormat,
+		Namespace:  opts.Namespace,
+		Subsystem:  opts.Subsystem,
+		Name:       opts.Name,
+		Help:       opts.Help,
+		LabelNames: opts.LabelNames,
+		LabelHelp:  opts.LabelHelp,
 	})}
 }
 
 func (p *provider) NewGauge(opts metrics.GaugeOpts) metrics.Gauge {
 	return &gauge{p.p.NewGauge(metrics2.GaugeOpts{
-		Namespace:    opts.Namespace,
-		Subsystem:    opts.Subsystem,
-		Name:         opts.Name,
-		Help:         opts.Help,
-		LabelNames:   opts.LabelNames,
-		LabelHelp:    opts.LabelHelp,
-		StatsdFormat: opts.StatsdFormat,
+		Namespace:  opts.Namespace,
+		Subsystem:  opts.Subsystem,
+		Name:       opts.Name,
+		Help:       opts.Help,
+		LabelNames: opts.LabelNames,
+		LabelHelp:  opts.LabelHelp,
 	})}
 }
 
 func (p *provider) NewHistogram(opts metrics.HistogramOpts) metrics.Histogram {
 	return &histogram{p.p.NewHistogram(metrics2.HistogramOpts{
-		Namespace:    opts.Namespace,
-		Subsystem:    opts.Subsystem,
-		Name:         opts.Name,
-		Help:         opts.Help,
-		Buckets:      opts.Buckets,
-		LabelNames:   opts.LabelNames,
-		LabelHelp:    opts.LabelHelp,
-		StatsdFormat: opts.StatsdFormat,
+		Namespace:  opts.Namespace,
+		Subsystem:  opts.Subsystem,
+		Name:       opts.Name,
+		Help:       opts.Help,
+		Buckets:    opts.Buckets,
+		LabelNames: opts.LabelNames,
+		LabelHelp:  opts.LabelHelp,
 	})}
 }
 
