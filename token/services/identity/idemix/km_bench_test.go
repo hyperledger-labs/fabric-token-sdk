0ea375ba38aff703843b53a521b9ecadd506d0bb
Angelo De Caro <adc@zurich.ibm.com>
2025-09-10 15:45:29 +0200

    rebase fix
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/services/identity/idemix/km_bench_test.go
+++ token/services/identity/idemix/km_bench_test.go
@@ -16,30 +16,31 @@ func BenchmarkKmIdentity(b *testing.B) {
 	b.Run("FP256BN_AMCL", func(b *testing.B) {
 		b.ReportAllocs()
 
-		keyManager, cleanup := setupKeyManager(b, "./testdata/fp256bn_amcl/idemix", math.FP256BN_AMCL, false)
+		keyManager, cleanup := setupKeyManager(b, "./testdata/fp256bn_amcl/idemix", math.FP256BN_AMCL)
 		defer cleanup()
 		for b.Loop() {
-			_, _, _ = keyManager.Identity(b.Context(), nil)
+			_, _ = keyManager.Identity(b.Context(), nil)
 		}
 	})
 
 	b.Run("BLS12_381_BBS", func(b *testing.B) {
 		b.ReportAllocs()
 
-		keyManager, cleanup := setupKeyManager(b, "./testdata/bls12_381_bbs/idemix", math.BLS12_381_BBS, true)
+		// in this case, the backed uses GURVY directly
+		keyManager, cleanup := setupKeyManager(b, "./testdata/bls12_381_bbs/idemix", math.BLS12_381_BBS)
 		defer cleanup()
 		for b.Loop() {
-			_, _, _ = keyManager.Identity(b.Context(), nil)
+			_, _ = keyManager.Identity(b.Context(), nil)
 		}
 	})
 
 	b.Run("BLS12_381_BBS_GURVY", func(b *testing.B) {
 		b.ReportAllocs()
 
-		keyManager, cleanup := setupKeyManager(b, "./testdata/bls12_381_bbs_gurvy/idemix", math.BLS12_381_BBS_GURVY, true)
+		keyManager, cleanup := setupKeyManager(b, "./testdata/bls12_381_bbs_gurvy/idemix", math.BLS12_381_BBS_GURVY)
 		defer cleanup()
 		for b.Loop() {
-			_, _, _ = keyManager.Identity(b.Context(), nil)
+			_, _ = keyManager.Identity(b.Context(), nil)
 		}
 	})
 }
--- token/services/identity/storage/kvs/hashicorp/go.mod
+++ token/services/identity/storage/kvs/hashicorp/go.mod
@@ -27,13 +27,11 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
-	github.com/consensys/gnark-crypto v0.18.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cockroachdb/errors v1.12.0 // indirect
 	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
 	github.com/cockroachdb/redact v1.1.5 // indirect
-	github.com/consensys/bavard v0.1.13 // indirect
-	github.com/consensys/gnark-crypto v0.13.0 // indirect
+	github.com/consensys/gnark-crypto v0.18.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
@@ -116,7 +114,7 @@ require (
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
