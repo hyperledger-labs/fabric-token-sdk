c8bb0aeba9e77e1bc6451c831ec92e4f3b6b7315
Hayim.Shaul@ibm.com <hayimsha@fhe3.haifa.ibm.com>
2025-11-05 05:43:20 -0500

    added unit tests
    Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@fhe3.haifa.ibm.com>

---
+++ token/services/identity/x509/crypto/io_test.go
@@ -0,0 +1,78 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package crypto
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func writeTempFile(t *testing.T, content []byte) string {
+	t.Helper()
+	dir := t.TempDir()
+	path := filepath.Join(dir, "test.pem")
+	err := os.WriteFile(path, content, 0600)
+	assert.NoError(t, err)
+	return path
+}
+
+func TestReadPemFile_ValidCert(t *testing.T) {
+
+	// Generate a private key
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	assert.NoError(t, err, "failed to generate RSA key")
+
+	// Create a simple certificate template
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			CommonName: "test.local",
+		},
+		NotBefore: time.Now().Add(-time.Hour),
+		NotAfter:  time.Now().Add(time.Hour),
+		KeyUsage:  x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+	}
+
+	// Self-sign the certificate
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	assert.NoError(t, err, "failed to create certificate")
+
+	// Encode certificate to PEM
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+
+	file := writeTempFile(t, certPEM)
+
+	_, err = readPemFile(file)
+	assert.NoError(t, err)
+}
+
+func TestReadPemFile_InvalidType(t *testing.T) {
+	pemData := `-----BEGIN UNKNOWN-----
+abcd
+-----END UNKNOWN-----`
+	file := writeTempFile(t, []byte(pemData))
+
+	_, err := readPemFile(file)
+	assert.Error(t, err, "expected error for unknown PEM type")
+}
+
+func TestReadPemFile_NoPemContent(t *testing.T) {
+	file := writeTempFile(t, []byte("not a pem file"))
+
+	_, err := readPemFile(file)
+	assert.Error(t, err, "expected error for non-PEM type")
+}
