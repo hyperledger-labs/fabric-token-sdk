96be3e450f02433407c8cb0876c69cbb03c74111
Angelo De Caro <adc@zurich.ibm.com>
2025-12-08 07:59:14 +0100

    curve extension + ipa use of mul2
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- token/core/zkatdlog/nogh/v1/crypto/rp/ipa.go
+++ token/core/zkatdlog/nogh/v1/crypto/rp/ipa.go
@@ -387,8 +387,7 @@ func innerProduct(left []*mathlib.Zr, right []*mathlib.Zr, c *mathlib.Curve) *ma
 func commitVector(left []*mathlib.Zr, right []*mathlib.Zr, leftgen []*mathlib.G1, rightgen []*mathlib.G1, c *mathlib.Curve) *mathlib.G1 {
 	com := c.NewG1()
 	for i := range left {
-		com.Add(leftgen[i].Mul(left[i]))
-		com.Add(rightgen[i].Mul(right[i]))
+		com.Add(leftgen[i].Mul2(left[i], rightgen[i], right[i]))
 	}
 	return com
 }
