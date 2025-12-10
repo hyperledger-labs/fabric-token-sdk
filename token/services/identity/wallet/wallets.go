4cfa1ca195f5199de532e5e0c4c7e950c4c728d9
Hayim.Shaul@ibm.com <hayimsha@fhe3.haifa.ibm.com>
2025-12-10 08:45:47 -0500

    remove unused var Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@fhe3.haifa.ibm.com>

--- token/services/identity/wallet/wallets.go
+++ token/services/identity/wallet/wallets.go
@@ -347,10 +347,10 @@ func (w *AnonymousOwnerWallet) RegisterRecipient(ctx context.Context, data *driv
 		return errors.Wrapf(err, "failed to match identity to audit infor for [%s]:[%s]", data.Identity, utils.Hashable(data.AuditInfo))
 	}
 	// register verifier and audit info
-	v, err := w.Deserializer.GetOwnerVerifier(ctx, data.Identity)
-	if err != nil {
-		return errors.Wrapf(err, "failed getting verifier for owner [%s]", data.Identity)
-	}
+	// v, err := w.Deserializer.GetOwnerVerifier(ctx, data.Identity)
+	// if err != nil {
+	// 	return errors.Wrapf(err, "failed getting verifier for owner [%s]", data.Identity)
+	// }
 	// if err := w.IdentityProvider.RegisterVerifier(ctx, data.Identity, v); err != nil {
 	// 	return errors.Wrapf(err, "failed registering verifier for owner [%s]", data.Identity)
 	// }
