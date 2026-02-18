02e8b2ecb26a4508de8535e904e9eb08978fb841
Effi-S <effi.szt@gmail.com>
2026-02-18 15:36:12 +0200

    Added GRPC
    Signed-off-by: Effi-S <effi.szt@gmail.com>

---
+++ cmd/benchmarking/api_grpc_bench_test.go
@@ -0,0 +1,42 @@
+/*
+Copyright IBM Corp All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package benchmarking
+
+import (
+	"path"
+	"testing"
+
+	"github.com/hyperledger-labs/fabric-smart-client/integration/benchmark/node"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/benchmark/flags"
+)
+
+var numConn = flags.IntSlice("numConn", []int{1, 2, 4, 8}, "Number of grpc client connections - may be a comma-separated list")
+
+// BenchmarkAPIGRPC exercises the ViewAPI via grpc client
+func BenchmarkAPIGRPC(b *testing.B) {
+	testdataPath := b.TempDir() // for local debugging you can set testdataPath := "out/testdata"
+	nodeConfPath := path.Join(testdataPath, "fsc", "nodes", "test-node.0")
+	clientConfPath := path.Join(nodeConfPath, "client-config.yaml")
+
+	// we generate our testdata
+	err := node.GenerateConfig(testdataPath)
+	require.NoError(b, err)
+
+	// create server
+	n, err := node.SetupNode(nodeConfPath, node.NamedFactory{
+		Name:    "zkp",
+		Factory: &TokenTxValidateViewFactory{},
+	})
+	require.NoError(b, err)
+
+	// run all workloads via direct view API
+	node.RunAPIGRPCBenchmark(b, zkpWorkload,
+		clientConfPath, *numConn)
+
+	n.Stop()
+}
