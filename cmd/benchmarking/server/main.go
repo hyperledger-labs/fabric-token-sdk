0c449abd1d42f7477e5542c6fbca520f486d0cda
Effi-S <effi.szt@gmail.com>
2026-02-18 16:04:10 +0200

    Added client-server bench
    Signed-off-by: Effi-S <effi.szt@gmail.com>

---
+++ cmd/benchmarking/server/main.go
@@ -0,0 +1,49 @@
+/*
+Copyright IBM Corp All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"path"
+
+	"github.com/hyperledger-labs/fabric-smart-client/integration/benchmark/node"
+	"github.com/hyperledger-labs/fabric-token-sdk/cmd/benchmarking"
+)
+
+func main() {
+	testdataPath := "./out/testdata" // for local debugging you can set testdataPath := "out/testdata"
+	nodeConfPath := path.Join(testdataPath, "fsc", "nodes", "test-node.0")
+
+	// we generate our testdata
+	err := node.GenerateConfig(testdataPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// create server
+	n, err := node.SetupNode(nodeConfPath, node.NamedFactory{
+		Name:    "zkp",
+		Factory: &benchmarking.TokenTxValidateViewFactory{},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Running fscnode %v\n", n.ID())
+
+	// Wait on OS terminate signal.
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+
+	n.Stop()
+
+	// cleanup generated data
+	_ = os.RemoveAll(testdataPath)
+}
