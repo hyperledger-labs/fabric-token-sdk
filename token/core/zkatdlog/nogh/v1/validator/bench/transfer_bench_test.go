81d206f40358860faa485c407b2a9ecf9b98dfa9
Effi-S <effi.szt@gmail.com>
2026-02-25 14:16:15 +0200

    Added Benchmark Transfer Service Benchmark
    Signed-off-by: Effi-S <effi.szt@gmail.com>

---
+++ token/core/zkatdlog/nogh/v1/validator/bench/transfer_bench_test.go
@@ -0,0 +1,93 @@
+/*
+Copyright IBM Corp All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package bench
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand/v2"
+	"sync/atomic"
+	"testing"
+
+	"github.com/hyperledger-labs/fabric-smart-client/integration/benchmark"
+	"github.com/hyperledger-labs/fabric-smart-client/platform/view/view"
+	"github.com/stretchr/testify/require"
+)
+
+// viewPool pre-generates distinct views so that benchmark iterations rotate
+// through different ZK proofs instead of repeatedly verifying same data.
+
+const (
+	defaultNumViews = 64
+)
+
+type viewPool struct {
+	views []view.View
+	idx   atomic.Int64
+}
+
+func shuffle[T any](s []T, noSeed bool, disable bool) {
+	if disable {
+		return
+	}
+	if !noSeed {
+		r := rand.New(rand.NewPCG(42, 54))
+		r.Shuffle(len(s), func(i, j int) {
+			s[i], s[j] = s[j], s[i]
+		})
+	} else {
+		rand.Shuffle(len(s), func(i, j int) {
+			s[i], s[j] = s[j], s[i]
+		})
+	}
+}
+
+func CreateViewsWithProofs(b *testing.B, testRoot string, f *TransferServiceViewFactory, n int) (*viewPool, []*trandferServiceParams) {
+	b.Helper()
+	vp := &viewPool{}
+
+	ps := NewTokenTransferVerifyParamsSlice(testRoot)
+	shuffle(ps, false, false)
+
+	// Create n views
+	vp.views = make([]view.View, n)
+	for i := range vp.views {
+		params := ps[i%len(ps)]
+
+		input, _ := json.Marshal(params)
+		v, err := f.NewView(input)
+		require.NoError(b, err)
+		vp.views[i] = v
+	}
+
+	vp.idx.Store(0)
+
+	return vp, ps
+}
+
+// nextView returns views from the pool in round-robin.
+func (vp *viewPool) nextView() view.View {
+	i := vp.idx.Add(1) - 1
+	l := len(vp.views)
+
+	return vp.views[i%int64(l)]
+}
+
+func BenchmarkTransferService(b *testing.B) {
+	f := &TransferServiceViewFactory{}
+	pool, params := CreateViewsWithProofs(b, "", f, defaultNumViews)
+
+	b.Run(fmt.Sprintf("out-tokens=%din-tokens=%d", params[0].NumOutputs, params[0].NumInputs), func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				_, _ = pool.nextView().Call(nil)
+			}
+		})
+		benchmark.ReportTPS(b)
+	})
+}
