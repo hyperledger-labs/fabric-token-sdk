62d170ee3afbc668dc9fbb83161a5c5d2a4175fe
Effi-S <effi.szt@gmail.com>
2026-02-25 17:02:20 +0200

    Added Benchmark Transfer Service Benchmark API GRPc and server-client setup with naive single params
    Signed-off-by: Effi-S <effi.szt@gmail.com>

---
+++ token/core/zkatdlog/nogh/v1/validator/bench/transfer_service/out/cmd/test-node/main.go
@@ -0,0 +1,20 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	fscnode "github.com/hyperledger-labs/fabric-smart-client/node"
+
+	
+)
+
+func main() {
+	n := fscnode.New()
+	n.Execute(func() error {
+		return nil
+	})
+}
