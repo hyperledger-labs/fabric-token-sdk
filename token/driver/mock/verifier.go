3b2ae28ce1521badfd1e8b211ef492274909e248
aaadir <adir@il.ibm.com>
2026-03-05 10:47:39 +0000

    deserializer unit tests
    Signed-off-by: aaadir <adir@il.ibm.com>

--- token/driver/mock/verifier.go
+++ token/driver/mock/verifier.go
@@ -99,8 +99,6 @@ func (fake *Verifier) VerifyReturnsOnCall(i int, result1 error) {
 func (fake *Verifier) Invocations() map[string][][]interface{} {
 	fake.invocationsMutex.RLock()
 	defer fake.invocationsMutex.RUnlock()
-	fake.verifyMutex.RLock()
-	defer fake.verifyMutex.RUnlock()
 	copiedInvocations := map[string][][]interface{}{}
 	for key, value := range fake.invocations {
 		copiedInvocations[key] = value
