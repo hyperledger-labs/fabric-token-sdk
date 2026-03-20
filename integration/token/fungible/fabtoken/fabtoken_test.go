236a5b5621b33abbdc7615c89ce239b3317784f2
Angelo De Caro <adc@zurich.ibm.com>
2026-03-20 06:51:42 +0100

    rebase fix + debug logs for fsc
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- integration/token/fungible/fabtoken/fabtoken_test.go
+++ integration/token/fungible/fabtoken/fabtoken_test.go
@@ -68,7 +68,7 @@ func newTestSuite(commType fsc.P2PCommunicationType, factor int, names ...string
 			SDKs:            []nodepkg.SDK{&ffabtoken.SDK{}},
 			ReplicationOpts: opts,
 			WebEnabled:      true, // Needed for the Remote Wallet with websockets
-			// FSCLogSpec:      "fsc.platform.view.services.comm=debug:info",
+			FSCLogSpec:      "fsc=debug:info",
 		},
 	))
 
