872173f1431f0fea7f96dd908870c884d8b08bb1
Angelo De Caro <adc@zurich.ibm.com>
2026-03-20 15:16:41 +0100

    fts debug
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- integration/token/fungible/fabtoken/fabtoken_test.go
+++ integration/token/fungible/fabtoken/fabtoken_test.go
@@ -68,7 +68,7 @@ func newTestSuite(commType fsc.P2PCommunicationType, factor int, names ...string
 			SDKs:            []nodepkg.SDK{&ffabtoken.SDK{}},
 			ReplicationOpts: opts,
 			WebEnabled:      true, // Needed for the Remote Wallet with websockets
-			FSCLogSpec:      "fsc=debug:info",
+			FSCLogSpec:      "fsc=debug:fts=debug:info",
 		},
 	))
 
