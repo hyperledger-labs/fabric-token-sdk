098c54c7347cc72337ddb568c05ee540b1ff71c8
Hayim.Shaul@ibm.com <hayimsha@fhe03.vpc.cloud9.ibm.com>
2026-06-15 07:17:50 +0000

    lint
    Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@fhe03.vpc.cloud9.ibm.com>

--- token/services/nfttx/wallet_unit_test.go
+++ token/services/nfttx/wallet_unit_test.go
@@ -61,13 +61,13 @@ type dummyTMSProviderSuccess struct{}
 
 func (d *dummyTMSProviderSuccess) GetTokenManagerService(opts driver.ServiceOptions) (driver.TokenManagerService, error) {
 	mockTMS := &driver_mock.TokenManagerService{}
-	
+
 	// Configure WalletService mock to return wallet instances
 	mockWS := &driver_mock.WalletService{}
 	mockWS.OwnerWalletReturns(&driver_mock.OwnerWallet{}, nil)
 	mockWS.IssuerWalletReturns(&driver_mock.IssuerWallet{}, nil)
 	mockWS.AuditorWalletReturns(&driver_mock.AuditorWallet{}, nil)
-	
+
 	mockTMS.WalletServiceReturns(mockWS)
 	mockTMS.ValidatorReturns(&driver_mock.Validator{}, nil)
 	mockAuth := &driver_mock.Authorization{}
