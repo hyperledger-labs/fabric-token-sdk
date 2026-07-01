564bca964dfcc954a0c5be5b9251811c19537fac
Hayim.Shaul@ibm.com <hayimsha@fhe03.vpc.cloud9.ibm.com>
2026-07-01 07:21:32 +0000

    fmt
    Signed-off-by: Hayim.Shaul@ibm.com <hayimsha@fhe03.vpc.cloud9.ibm.com>

--- token/services/selector/simple/selector_test.go
+++ token/services/selector/simple/selector_test.go
@@ -98,7 +98,7 @@ func (r *recordingLocker) UnlockIDs(_ context.Context, ids ...*token2.ID) []*tok
 		copy(cp, ids)
 		r.unlocked = append(r.unlocked, cp)
 	}
-	
+
 	return nil
 }
 
@@ -111,7 +111,7 @@ func (r *recordingLocker) totalUnlocked() []*token2.ID {
 	for _, group := range r.unlocked {
 		out = append(out, group...)
 	}
-	
+
 	return out
 }
 
